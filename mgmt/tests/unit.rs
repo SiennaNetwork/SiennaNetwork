47bb41b07023e6743082bc69f2c12150dffaf4f2
Adam Avramov <adam@hack.bg>
2021-01-22 18:52:04 +0200

    add test helper macro

--- mgmt/tests/unit.rs
+++ mgmt/tests/unit.rs
@@ -1,3 +1,5 @@
+#[macro_use] extern crate kukumba;
+
 use cosmwasm_std::{
     Api, Env, BlockInfo, MessageInfo, ContractInfo,
     coins, from_binary,
@@ -62,166 +64,129 @@ macro_rules! tx {
     }
 }
 
-#[test] fn init () {
-
-    let alice: HumanAddr = "Alice".into();
-    let mut deps = harness(&[(&alice, &coins(1000, "SIENNA")),]);
-
-    // When  I init the contract
-    // Then  I should become admin
-    // And   I should be able to query its state
-    // And   it should not be launched
-    query!(deps StatusQuery (res: StatusResponse) {
-        assert_eq!(res.launched, None)
-    });
-}
-
-#[test] fn launch () {
-
-    let alice:   HumanAddr = "Alice".into();
-    let mallory: HumanAddr = "Mallory".into();
-    let mut deps = harness(&[
-        (&alice,   &coins(1000, "SIENNA")),
-        (&mallory, &coins(   0, "SIENNA"))
-    ]);
-
-    // Given the contract IS NOT YET launched
-
-    // As    a stranger
-    // When  I try to launch the contract
-    // Then  I should fail
-    let env = mock_env(1, 1, &mallory, coins(0, "SIENNA"));
-    println!("{:#?}", env);
-    tx!(deps env Launch);
-    query!(deps StatusQuery (res: StatusResponse) {
-        assert_eq!(res.launched, None)
-    });
-
-    // As    the admin
-    // When  I launch the contract
-    // Then  it should remember when it was first launched
-    let env = mock_env(2, 2, &alice, coins(0, "SIENNA"));
-    tx!(deps env Launch);
-    query!(deps StatusQuery (res: StatusResponse) {
-        assert_eq!(res.launched, Some(2))
-    });
-
-    // Given the contract IS ALREADY launched
-
-    // As    the admin
-    // When  I launch the contract
-    // Then  it should say it's already launched
-    // And   it should not update its launch date
-    let env = mock_env(3, 3, &alice, coins(0, "SIENNA"));
-    tx!(deps env Launch);
-    query!(deps StatusQuery (res: StatusResponse) {
-        assert_eq!(res.launched, Some(2))
-    });
-}
-
-#[test] fn configure () {
-
-    let alice:   HumanAddr = "Alice".into();
-    let bob:     HumanAddr = "Bob".into();
-    let mallory: HumanAddr = "Mallory".into();
-    let mut deps = harness(&[
-        (&alice,   &coins(1000, "SIENNA")),
-        (&bob,     &coins(   0, "SIENNA")),
-        (&mallory, &coins(   0, "SIENNA"))
-    ]);
-
-    // Given the contract IS NOT YET launched
-
-    // As    the admin
-    // When  I set the recipients
-    // Then  I should be able to fetch them
-    let env = mock_env(1, 1, &alice, coins(1000, "SIENNA"));
-    tx!(deps env SetRecipients { recipients: vec![
-        mgmt::Recipient {
-            address:  deps.api.canonical_address(&bob).unwrap(),
-            cliff:    0,
-            vestings: 10,
-            interval: 10,
-            claimed:  0
-        }
-    ] });
-
-    // As    a stranger
-    // When  I try to set the recipients
-    // Then  I should be denied access
-    let env = mock_env(2, 2, &mallory, coins(0, "SIENNA"));
-
-    // Given the contract IS ALREADY launched
-
-    // As    the admin
-    // When  I try to set the recipients
-    // Then  I should be denied access
-    let env = mock_env(3, 3, &alice, coins(1000, "SIENNA"));
-
-    // As    a stranger
-    // When  I try to set the recipients
-    // Then  I should be denied access
-    let env = mock_env(4, 4, &mallory, coins(0, "SIENNA"));
-}
-
-#[test] fn claim () {
-
-    let alice:   HumanAddr = "Alice".into();
-    let bob:     HumanAddr = "Bob".into();
-    let mallory: HumanAddr = "Mallory".into();
-    let mut deps = harness(&[
-        (&alice,   &coins(1000, "SIENNA")),
-        (&bob,     &coins(   0, "SIENNA")),
-        (&mallory, &coins(   0, "SIENNA"))
-    ]);
-
-    // Given the contract IS NOT YET launched
-    // As    a stranger
-    // When  I try to claim funds
-    // Then  I should be denied
-    // As    a claimant
-    // When  I try to claim funds
-    // Then  I should be denied
-
-    // Given the contract IS ALREADY launcher
-    // As    a stranger
-    // When  I try to claim funds
-    // Then  I should be denied
-    // As    a claimant
-    // When  I try to claim funds
-    // Then  the contract should transfer them to my address
-    // And   the contract should update how much I've claimed
-}
-
-/*
-
 kukumba!(
 
+    #[init]
+    given "the contract is not yet deployed" {
+        let alice: HumanAddr = "Alice".into();
+        let mut deps = harness(&[(&alice, &coins(1000, "SIENNA")),]);
+    }
+
+    when "someone deploys the contract" {
+        let res = mgmt::init(
+            &mut deps,
+            mock_env(0, 0, &alice, coins(1000, "SIENNA")),
+            mgmt::msg::InitMsg { token: None }
+        ).unwrap();
+    }
+
+    then "they become admin"
+    and  "they should be able to query its state"
+    and  "it should say it's not launched yet" {
+        query!(deps StatusQuery (res: StatusResponse) {
+            assert_eq!(res.launched, None)
+        });
+    }
+
+    #[launch]
+    given "the contract is not yet launched" {
+        let alice:   HumanAddr = "Alice".into();
+        let mallory: HumanAddr = "Mallory".into();
+        let mut deps = harness(&[
+            (&alice,   &coins(1000, "SIENNA")),
+            (&mallory, &coins(   0, "SIENNA"))
+        ]);
+    }
+
+    when "a stranger tries to start the vesting"
+    then "they should fail" {
+        let env = mock_env(1, 1, &mallory, coins(0, "SIENNA"));
+        println!("{:#?}", env);
+        tx!(deps env Launch);
+        query!(deps StatusQuery (res: StatusResponse) {
+            assert_eq!(res.launched, None)
+        });
+    }
+
+    when "the admin tries to start the vesting" {
+        let env = mock_env(2, 2, &alice, coins(0, "SIENNA"));
+        tx!(deps env Launch);
+    }
+    then "the contract should remember that moment" {
+        query!(deps StatusQuery (res: StatusResponse) {
+            assert_eq!(res.launched, Some(2))
+        });
+    }
+
+    given "the contract is already launched"
+    when "the admin tries to start the vesting"
+    then "the contract should say it's already launched"
+    and "it should not update its launch date" {
+        let env = mock_env(3, 3, &alice, coins(0, "SIENNA"));
+        tx!(deps env Launch);
+        query!(deps StatusQuery (res: StatusResponse) {
+            assert_eq!(res.launched, Some(2))
+        });
+    }
+
+    #[configure]
+    given "the contract is not yet launched" {
+        let alice:   HumanAddr = "Alice".into();
+        let bob:     HumanAddr = "Bob".into();
+        let mallory: HumanAddr = "Mallory".into();
+        let mut deps = harness(&[
+            (&alice,   &coins(1000, "SIENNA")),
+            (&bob,     &coins(   0, "SIENNA")),
+            (&mallory, &coins(   0, "SIENNA"))
+        ]);
+    }
+
+    when "the admin sets the recipients"
+    then "they should be updated" {
+        let env = mock_env(1, 1, &alice, coins(1000, "SIENNA"));
+        tx!(deps env SetRecipients { recipients: vec![
+            mgmt::Recipient {
+                address:  deps.api.canonical_address(&bob).unwrap(),
+                cliff:    0,
+                vestings: 10,
+                interval: 10,
+                claimed:  0
+            }
+        ] });
+        todo!()
+    }
+
+    when "a stranger tries to set the recipients" 
+    then "they should be denied access" { todo!() }
+
+    given "the contract is already launched" {}
+
+    when "the admin tries to set the recipients" {
+        let env = mock_env(3, 3, &alice, coins(1000, "SIENNA"));
+    }
+    then "they should be denied access" { todo!() }
+
+    when "a stranger tries to set the recipients" {
+        let env = mock_env(4, 4, &mallory, coins(0, "SIENNA"));
+    }
+    then "they should be denied access" { todo!() }
+
+    #[claim]
     given "the contract is not yet launched"
 
-        as "a stranger" [ 0 SIENNA, 0 SCRT ]
-            when "I try to launch the contract"
-                tx Launch;
-            then "I should fail"
-                q StatusQuery (res: StatusResponse)
-                    assert_eq!(res.launched, None)
+    when "a stranger tries to claim funds"
+    then "they should be denied" { todo!() }
 
-        as "the admin" [ 1000 SIENNA, 1000 SCRT ]
-            when "I launch the contract" {
-                let time1 = env.block.time;
-                tx Launch;
-            }
-            then "it should remember that moment"
-                q StatusQuery (res: StatusResponse)
-                    assert_eq!(res.launched, time1)
+    when "a claimant tries to claim funds" {}
+    then "they should be denied" { todo!() }
 
     given "the contract is already launched"
 
-        as "the admin" [ 1000 SIENNA, 1000 SCRT ]
-            when "I try to launch the contract again" {}
-            then "it should say it's already launched" {}
-            and  "it should not update its launch date" {}
+    when "a stranger tries to claim funds"
+    then "they should be denied" { todo!() }
+
+    when "a claimant tries to claim funds"
+    then "the contract should transfer them to their address" { todo!() }
+    and  "the contract should remember how much I've claimed so far"  { todo!() }
 
 );
-
-*/
