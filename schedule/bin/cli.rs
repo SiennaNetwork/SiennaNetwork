46556eec7353dfc323345c2c27bf7dafd3b09948
Adam Avramov <adam@hack.bg>
2021-03-02 00:03:58 +0200

    refactor(schedule): define final template format

--- schedule/bin/cli.rs
+++ schedule/bin/cli.rs
@@ -23,26 +23,26 @@ fn main () {
                 .short('f').long("force").takes_value(false)
                 .about("Overwrite the destination file if it exists"))
 
-            .arg(Arg::new("OUTPUT")
-                .default_value("schedule.ods")))
+            .arg(Arg::new("PATH")
+                .default_value("./schedule.ods")))
 
         .subcommand(App::new("validate")
             .about("Validate a configuration")
 
-            .arg(Arg::new("INPUT")
+            .arg(Arg::new("PATH")
                 .default_value("schedule.ods")))
 
         .subcommand(App::new("render")
             .about("Generate a portion list from a configuration")
 
-            .arg(Arg::new("INPUT")
+            .arg(Arg::new("PATH")
                 .default_value("schedule.ods")))
 
         .get_matches();
 
     subcommand!(
         matches "template" {
-            let output = matches.value_of("OUTPUT").unwrap();
+            let output = matches.value_of("PATH").unwrap();
             let path = std::path::Path::new(&output);
             if path.exists() && !matches.is_present("force") {
                 panic!("{} already exists, refusing to overwrite", &output);
@@ -51,11 +51,15 @@ fn main () {
         }
 
         matches "validate" {
-            println!("VALIDATE {:#?}", &matches);
+            let input = matches.value_of("PATH").unwrap();
+            let path = std::path::Path::new(&input);
+            println!("VALIDATE {:#?}", &path);
         }
 
         matches "render" {
-            println!("RENDER {:#?}", &matches);
+            let input = matches.value_of("PATH").unwrap();
+            let path = std::path::Path::new(&input);
+            println!("RENDER {:#?}", &path);
         }
     );
 }
@@ -68,29 +72,32 @@ fn generate_template () -> spreadsheet_ods::WorkBook {
     };
     let mut book     = WorkBook::new();
     let mut sheet    = Sheet::new();
-    let mut schedule = Schedule!(1000
-        (Founders  500 (Founder1  250) (Founder2  250))
-        (Investors 500 (Investor1 250) (Investor2 250))
+    let mut schedule = Schedule!(
+        1000
+        (Founders  500 (Founder1  250)
+                       (Founder2  250))
+        (Investors 500 (Investor1 250)
+                       (Investor2 250))
     );
 
     let (st_header, st_schedule, st_pool, st_account) = add_cell_styles(&mut book);
 
     macro_rules! zone {
-        ($row:expr, $col:expr, $rows:expr, $cols:expr, $style:expr) => {
+        ( $style:expr
+        ; $row:expr
+        ; $col:expr
+        ; $rows:expr
+        ; $cols:expr
+        $(; $(($value_row:expr, $value_col:expr, $value:expr)),*)? ) => {
             for row in $row..$row+$rows {
                 for col in $col..$col+$cols {
                     println!("{} {} <{}> = {}", row, col, stringify!($style), "");
                     sheet.set_styled_value(row, col, " ", &$style);
                 }
             }
-        }
+            $(add_values!($(($value_row, $value_col, $value, $style),)*))?
+        };
     }
-
-    zone!(0, 0,  1, 22, header);
-    zone!(1, 0, 10, 22, schedule);
-    zone!(2, 1,  9, 21, pool);
-    zone!(3, 3,  3, 21, account);
-    zone!(7, 3,  3, 21, account);
 
     macro_rules! add_values {
         ($(( $row:expr, $col:expr, $value:expr, $style:expr ),)*) => {
@@ -101,90 +108,43 @@ fn generate_template () -> spreadsheet_ods::WorkBook {
         };
     };
 
-    add_values!(
-        (0, 0, "Total",  header),
-        (1, 0, 10000000, schedule),
+    zone!(st_header // style
+         ;0         // starting row
+         ;0         // starting column
+         ;2         // height
+         ;22        // width
+         ;          // items
+        (0,  0, "Schedule"),
+        (1,  0, "Total"),
 
-        (0, 1, "Pool",     header),
-        (2, 1, "Founders", pool),
+        (0,  1, "Pool"),
+        (1,  1, "Name"),
+        (1,  2, "Subtotal"),
 
-        (0, 2, "Subtotal", header),
-        (1, 2, 10000000,   schedule),
-        (2, 2, 10000000,   pool),
+        (0,  3, "Account"),
+        (1,  3, "Name"),
+        (1,  4, "Amount"),
+        (1,  5, "% of total"),
+        (1,  7, "Days"),
+        (1,  8, "(seconds)"),
+        (1, 12, "SIENNA"),
 
-        (0, 3, "Account",  header),
-        (3, 3, "Founder3", account),
+        (0, 13, "Allocation"),
+        (1, 13, "Address"),
+        (1, 14, "Amount"));
 
-        (0,  4, "Amount", header),
-        (3,  4,   731000, account),
-
-        (0,  5, "% of Total", header),
-        (2,  5, "100%",       pool),
-        (3,  5, "100%",       account),
-
-        (0,  6, "Start at\nNth day", header),
-        (3,  6, 180,                 account),
-
-        (0,  7, "`start_at`\n(seconds)", header),
-        (3,  7, 15552000,                account),
-
-        (0,  8, "Interval\n(days)", header),
-        (3,  8, 1,                  account),
-
-        (0,  9, "`interval`\n(seconds)", header),
-        (3,  9, 86400,                   account),
-
-        (0, 10, "Duration\n(days)", header),
-        (3, 10, 481,                account),
-
-        (0, 11, "`duration`\n(seconds)", header),
-        (3, 11, 41558400,                account),
-
-        (0, 12, "Cliff\n%", header),
-        (3, 12, 10.0,       account),
-
-        (0, 13, "`cliff`\n(SIENNA)", header),
-        (3, 13, 73100,               account),
-
-        (0, 14, "Cliff\nallocation", header),
-        (3, 14, 4000,                account),
-        (4, 14, 166,                 account),
-
-        (0, 15, "Cliff\nrecipient", header),
-        (3, 15, "secret1MyAddressxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", account),
-        (4, 15, "secret1MyOtherAddressxxxxxxxxxxxxxxxxxxxxxxxx", account),
-
-        (0, 16, "Regular\nportions", header),
-        (3, 16, 480,                 account),
-
-        (0, 17, "Portion\nsize", header),
-        (3, 17, 137, account),
-
-        (0, 18, "Portion\nallocation", header),
-        (3, 18, 1000, account),
-        (4, 18, 370, account),
-
-        (0, 19, "Portion\nrecipient", header),
-        (3, 19, "secret1MyAddressxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", account),
-        (4, 19, "secret1MyOtherAddressxxxxxxxxxxxxxxxxxxxxxxxx", account),
-
-        (0, 20, "Remainder", header),
-        (3, 20, 4166, account),
-
-        (0, 21, "Remainder\nallocation", header),
-        (3, 21, 4000, account),
-        (4, 21, 166, account),
-
-        (0, 22, "Remainder\nrecipient", header),
-        (3, 22, "secret1MyAddressxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", account),
-        (4, 22, "secret1MyOtherAddressxxxxxxxxxxxxxxxxxxxxxxxx", account),
-    );
+    zone!(st_schedule ; 2 ; 0 ; 10 ; 22);
+    zone!(st_pool     ; 3 ; 1 ;  9 ; 21);
+    zone!(st_account  ; 4 ; 3 ;  3 ; 21);
+    zone!(st_account  ; 8 ; 3 ;  3 ; 21);
 
     book.push_sheet(sheet);
     book
 }
 
-fn add_cell_styles (&mut book: spreadsheet_ods::WorkBook) {
+use spreadsheet_ods::CellStyleRef;
+fn add_cell_styles (book: &mut spreadsheet_ods::WorkBook)
+    -> (CellStyleRef, CellStyleRef, CellStyleRef, CellStyleRef) {
     use color::Rgb;
     use spreadsheet_ods::{
         CellStyle,
