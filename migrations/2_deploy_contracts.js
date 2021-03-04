7d3c682953a999891c5e29c1ddf199a9b1f1773d
Milen Radkov <milenradkov@me.com>
2021-03-04 12:27:45 +0200

    chore(docs): add comment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,6 @@
 const WrappedSienna = artifacts.require('WrappedSienna');
-const MockSienna = artifacts.require('MockSienna');
 
 module.exports = function (deployer, networks, accounts) {
-  const bridgeAddress = '0x2b89bf8ba858cd2fcee1fada378d5cd6936968be';
-
-  //deployer.deploy(WrappedSienna, bridgeAddress); //TODO: add after to test with real network
+  // TODO: use real bridge address for e2e tests
   deployer.deploy(WrappedSienna, accounts[0]);
 };
