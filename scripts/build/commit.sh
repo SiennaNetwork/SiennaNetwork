83f6385dc170b0c15ce0fcd2f07e664b599db2f8
Adam Avramov <adam@hack.bg>
2021-03-05 15:26:45 +0200

    build(mgmt): ungzip older artifacts

--- scripts/build/commit.sh
+++ scripts/build/commit.sh
@@ -18,15 +18,16 @@ for Contract in sienna-mgmt snip20-reference-impl; do
     -v $HOME/.ssh/known_hosts:/root/.ssh/known_hosts:ro           \
     --mount type=volume,source=sienna_cache_$Commit,target=/code/target               \
     --mount type=volume,source=registry_cache_$Commit,target=/usr/local/cargo/registry \
-    --entrypoint /bin/sh                   \
-    hackbg/secret-contract-optimizer:latest \
-    -c "mkdir -p /contract && cd /contract  &&\
-        git clone --recursive -n $Origin . &&\
-        git checkout $Commit              &&\
-        git submodule update             &&\
-        chown -R 1000 /contract         &&\
-        /entrypoint.sh $Contract       &&\
-        ls -al && mv $Contract.wasm /output/"
+    --entrypoint /bin/sh                       \
+    hackbg/secret-contract-optimizer:latest     \
+    -c "mkdir -p /contract && cd /contract     &&\
+        git clone --recursive -n $Origin .      &&\
+        git checkout $Commit                     &&\
+        git submodule update                      &&\
+        chown -R 1000 /contract                    &&\
+        /entrypoint.sh $Contract                    &&\
+        ls -al && (gzip -df $Contract.wasm.gz||true) &&\
+        mv $Contract.wasm /output/"
   ls -al && mv "$Contract.wasm" "dist/$Commit-$Contract.wasm"; done
 cd dist
 sha256sum -b *.wasm > checksums.sha256.txt
