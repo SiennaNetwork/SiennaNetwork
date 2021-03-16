182a06795e28492bc380aacfdbc3c2f8100a12af
Adam Avramov <adam@hack.bg>
2021-03-17 00:19:34 +0200

    feat(schedule): add account to pool via schedule

--- libraries/schedule/lib/mutate.rs
+++ libraries/schedule/lib/mutate.rs
@@ -1,7 +1,12 @@
 use crate::*;
 impl Schedule {
-    pub fn add_account (&self, pool_name: String, account: Account) -> UsuallyOk {
-        unimplemented!()
+    pub fn add_account (&mut self, pool_name: String, account: Account) -> UsuallyOk {
+        for pool in self.pools.iter_mut() {
+            if pool.name == pool_name {
+                return pool.add_account(account)
+            }
+        }
+        self.err_pool_not_found(pool_name)
     }
 }
 impl Pool {
@@ -17,6 +22,7 @@ impl Pool {
         if self.unallocated() == 0 {
             self.partial = false
         }
+        self.validate()?;
         Ok(())
     }
 }
@@ -37,7 +43,7 @@ mod tests {
         let Alice = HumanAddr::from("Alice");
         let Bob   = HumanAddr::from("Bob");
         let Carol = HumanAddr::from("Carol");
-        let mut P = Pool::partial("P", 100, &[Account::immediate("A", &Alice, 100)]);
+        let mut P = Pool::partial("P", 200, &[Account::immediate("A", &Alice, 100)]);
         assert_eq!(P.add_account(Account::immediate("B", &Bob, 200)),
                    P.err_account_too_big(&Account::immediate("B", &Bob, 200)));
         assert_eq!(P.partial,
@@ -48,5 +54,25 @@ mod tests {
                    false);
         assert_eq!(P.add_account(Account::immediate("C", &Carol, 1)),
                    P.err_pool_full());
+    }
+    #[test] fn test_add_to_schedule () {
+        let Alice = HumanAddr::from("Alice");
+        let Bob   = HumanAddr::from("Bob");
+        let Carol = HumanAddr::from("Carol");
+        let mut S = Schedule::new(&[
+            Pool::partial("P1", 100, &[]),
+            Pool::full("P2", &[Account::immediate("A", &Alice, 100)]),
+        ]);
+        assert_eq!(S.add_account("P1".to_string(), Account::immediate("B", &Bob, 50)),
+                   Ok(()));
+        let A = Account::immediate("B", &Bob, 100);
+        assert_eq!(S.add_account("P1".to_string(), A.clone()),
+                   S.pools.get(0).unwrap().err_account_too_big(&A));
+        assert_eq!(S.add_account("P1".to_string(), Account::immediate("C", &Carol, 50)),
+                   Ok(()));
+        assert_eq!(S.add_account("P1".to_string(), A.clone()),
+                   S.pools.get(0).unwrap().err_pool_full());
+        assert_eq!(S.add_account("P2".to_string(), A.clone()),
+                   S.pools.get(1).unwrap().err_pool_full());
     }
 }
