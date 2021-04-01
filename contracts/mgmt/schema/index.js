4c3cbd2a77532b0fb9a8593286baec1f6183a352
Adam Avramov <adam@hack.bg>
2021-04-01 07:18:28 +0300

    chore(mgmt, rpt): regenerate schema

--- contracts/mgmt/schema/index.js
+++ contracts/mgmt/schema/index.js
@@ -8,33 +8,30 @@ export const schema = loadSchemas(import.meta.url, {
 })
 
 export default class MGMT extends SecretNetwork.Contract.withSchema(schema) {
-
   // query contract status
   get status () { return this.q.status() }
-
   // query current schedule
   get schedule () { return this.q.get_schedule() }
-
   // take over a SNIP20 token
   acquire = async snip20 => {
     await snip20.setMinters([this.address])
     await snip20.changeAdmin(this.address)
     return this
   }
-
   // load a schedule
   configure = schedule => this.tx.configure({ schedule })
-
   // launch the vesting
-  launch = () => this.tx.launch()
-
+  launch = () =>
+    this.tx.launch().then(result=>({
+      launched: result.logs[0].events[1].attributes[1].value
+    }))
   // claim accumulated portions
-  claim = claimant => this.tx.claim({}, claimant)
-
+  claim = claimant =>
+    this.tx.claim({}, claimant)
   // see how much is claimable by someone at a certain time
-  progress = (address, time = + new Date() / 1000) => this.q.progress({ address, time })
-
+  progress = (address, time = + new Date()) =>
+    this.q.progress({ address, time: Math.floor(time  / 1000) })
   // add a new account to a pool
-  add = (pool, account) => this.tx.add_account({ pool, account })
-
+  add = (pool, account) =>
+    this.tx.add_account({ pool, account })
 }
