b748fd1b467430899ff3791fa0085f98e7444731
Adam Avramov <adam@hack.bg>
2021-04-05 14:51:54 +0300

    feat(js): add setViewingKey

--- schema/index.js
+++ schema/index.js
@@ -8,6 +8,9 @@ export const schema = loadSchemas(import.meta.url, {
   handleAnswer: './handle_answer.json'
 })
 
+const decoder = new TextDecoder()
+const decode = buffer => decoder.decode(buffer).trim()
+
 export default class SNIP20 extends SecretNetwork.Contract.withSchema(schema) {
 
   setMinters = minters =>
@@ -16,13 +19,21 @@ export default class SNIP20 extends SecretNetwork.Contract.withSchema(schema) {
   changeAdmin = address =>
     this.tx.change_admin({address})
 
-  createViewingKey = (agent, entropy = "minimal", address = agent.address) =>
-    this.tx.create_viewing_key({address, entropy}, agent)
-      .then(({data})=>JSON.parse(data).create_viewing_key.key)
-      // TODO automatically parse+validate response (in @hackbg/fadroma)
+  async createViewingKey (agent, entropy = "minimal", address = agent.address) {
+    const {data, transactionHash: tx} = await this.tx.create_viewing_key({key}, agent)
+    const {key} = JSON.parse(decode(data)).create_viewing_key
+    return {tx, key}
+  }
 
-  balance = (agent, key, address = agent.address) =>
-    this.q.balance({key, address}, agent)
-      .then(({balance:{amount}})=>amount)
+  async setViewingKey (agent, key, address = agent.address) {
+    const {data, transactionHash: tx} = await this.tx.set_viewing_key({key}, agent)
+    const {status} = JSON.parse(decode(data)).set_viewing_key
+    return {tx, status}
+  }
+
+  async balance (agent, key, address = agent.address) {
+    const {balance:{amount}} = await this.q.balance({key, address}, agent)
+    return amount
+  }
 
 }
