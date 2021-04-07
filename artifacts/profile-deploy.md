84daf6a196712b50cfe4b1dfe12bcdd299fb4cc0
Adam Avramov <adam@hack.bg>
2021-04-07 08:43:32 +0300

    tag 2.0.0-rc1

--- artifacts/profile-deploy.md
+++ artifacts/profile-deploy.md
@@ -1,21 +1,11 @@
 | time | info | time (msec) | gas (uSCRT) | overhead (msec) |
 | --- | --- | --- | --- | --- |
-| 2021-04-07T01:35:04.118Z | allow adding accounts to Advisors pool in place of AdvisorN | 0 |
-| 2021-04-07T01:35:04.118Z | shorten schedule and replace placeholders with test accounts | 108 |
-| 2021-04-07T01:35:04.226Z | create extra test accounts for reallocation tests | 24 |
-| 2021-04-07T01:35:04.258Z | build mgmt | 3320 |
-| 2021-04-07T01:35:04.258Z | build token | 3409 |
-| 2021-04-07T01:35:04.259Z | build rpt | 10016 |
-| 2021-04-07T01:35:04.259Z | build project | 10016 |
-| 2021-04-07T01:35:14.276Z | upload token | 3 |
-| 2021-04-07T01:35:14.279Z | upload mgmt | 2 |
-| 2021-04-07T01:35:14.281Z | upload rpt | 1 |
-| 2021-04-07T01:35:14.283Z | initialize token | 3036 | 140973 | 6 |
-| 2021-04-07T01:35:17.325Z | initialize mgmt | 5033 | 238159 | 4 |
-| 2021-04-07T01:35:22.362Z | initialize rpt | 5043 | 134742 | 4 |
-| 2021-04-07T01:35:27.409Z | create 16 recipient accounts | 4974 | 333072 | 7 |
-| 2021-04-07T01:35:32.390Z | set null viewing keys | 6254 | 1720150 | 31 |
-| 2021-04-07T01:35:38.675Z | make mgmt owner of token | 8942 | 217735 | 12 |
-| 2021-04-07T01:35:47.629Z | point RPT account in schedule to RPT contract | 5008 | 240155 | 115 |
-| 2021-04-07T01:35:52.752Z | launch the vesting | 5048 | 313959 | 3 |
-|  | **total** | 70237 | 3338945 | 182 |
+| 2021-04-07T05:33:08.224Z | allow adding accounts to Advisors pool in place of AdvisorN | 0 |
+| 2021-04-07T05:33:08.224Z | shorten schedule and replace placeholders with test accounts | 87 |
+| 2021-04-07T05:33:08.312Z | create extra test accounts for reallocation tests | 27 |
+| 2021-04-07T05:33:24.098Z | create 16 recipient accounts | 4980 | 333072 | 112 |
+| 2021-04-07T05:33:29.191Z | set null viewing keys | 6084 | 1720022 | 19 |
+| 2021-04-07T05:33:35.294Z | make mgmt owner of token | 8984 | 217719 | 5 |
+| 2021-04-07T05:33:44.283Z | point RPT account in schedule to RPT contract | 5038 | 240147 | 107 |
+| 2021-04-07T05:33:49.428Z | launch the vesting | 5062 | 313935 | 3 |
+|  | **total** | 30262 | 2824895 | 246 |
--- demo.js
+++ demo.js
@@ -24,9 +24,9 @@ const __dirname = fileURLToPath(dirname(import.meta.url))
 
 export default async function demo ({network, agent, builder}) {
 
-  const task = taskmaster({
+  const deployTask = taskmaster({
     header: [ 'time', 'info', 'time (msec)', 'gas (uSCRT)', 'overhead (msec)' ],
-    output: resolve(__dirname, 'docs', 'gas-report.md'),
+    output: resolve(__dirname, 'artifacts', 'profile-deploy.md'),
     agent
   })
 
@@ -38,7 +38,7 @@ export default async function demo ({network, agent, builder}) {
 
   async function prepare (chain, agent, schedule) {
 
-    await task('allow adding accounts to Advisors pool in place of AdvisorN', () => {
+    await deployTask('allow adding accounts to Advisors pool in place of AdvisorN', () => {
       for (const pool of schedule.pools) {
         if (pool.name === 'Advisors') {
           pool.partial = true
@@ -56,7 +56,7 @@ export default async function demo ({network, agent, builder}) {
     const wallets    = []
         , recipients = {}
 
-    await task('shorten schedule and replace placeholders with test accounts', async () => {
+    await deployTask('shorten schedule and replace placeholders with test accounts', async () => {
       await Promise.all(schedule.pools.map(pool=>Promise.all(pool.accounts.map(mutateAccount))))
       async function mutateAccount (account) {
         // * create an agent for each recipient address (used to test claims)
@@ -74,7 +74,7 @@ export default async function demo ({network, agent, builder}) {
       }
     })
 
-    await task('create extra test accounts for reallocation tests', async () => {
+    await deployTask('create extra test accounts for reallocation tests', async () => {
       const extras = [ 'NewAdvisor', 'TokenPair1', 'TokenPair2', 'TokenPair3', ]
       for (const name of extras) {
         const extra = await chain.getAgent(name) // create agent
@@ -93,10 +93,10 @@ export default async function demo ({network, agent, builder}) {
       buildImage: 'enigmampc/secret-contract-optimizer:latest',
       buildUser:  'root',
       outputDir:  resolve(workspace, 'artifacts'), })
-    const binaries  = await build({ task, builder })
-    const receipts  = await upload({ task, builder, binaries })
+    const binaries  = await build({ deployTask, builder })
+    const receipts  = await upload({ deployTask, builder, binaries })
     const timestamp = String(+ new Date())
-    const contracts = await initialize({ task, agent, receipts, inits: {
+    const contracts = await initialize({ deployTask, agent, receipts, inits: {
       TOKEN: { label:  `[${timestamp}] snip20`
              , initMsg: { name:      "Sienna"
                         , symbol:    "SIENNA"
@@ -107,8 +107,7 @@ export default async function demo ({network, agent, builder}) {
       MGMT:  { label:  `[${timestamp}] mgmt`
              , initMsg: { schedule } },
       RPT:   { label:  `[${timestamp}] rpt`
-             , initMsg: { pool:      'MintingPool'
-                        , account:   'RPT'
+             , initMsg: { portion:   "2500000000000000000000"
                         , config:    [[recipients.TokenPair1.address, "2500000000000000000000"]]}} } })
     return contracts
   }
@@ -117,25 +116,25 @@ export default async function demo ({network, agent, builder}) {
 
     const { TOKEN, MGMT, RPT } = contracts
 
-    await task(`create ${wallets.length} recipient accounts`, async report => {
+    await deployTask(`create ${wallets.length} recipient accounts`, async report => {
       const tx = await agent.sendMany(wallets, 'create recipient accounts')
       report(tx.transactionHash)
     })
 
     const VK = ""
-    await task('set null viewing keys', async report => {
+    await deployTask('set null viewing keys', async report => {
       let txs = Object.values(recipients).map(({agent})=>TOKEN.setViewingKey(agent, VK))
       txs = await Promise.all(txs)
       for (const {tx} of txs) report(tx.transactionHash)
     })
 
-    await task('make mgmt owner of token', async report => {
+    await deployTask('make mgmt owner of token', async report => {
       const [tx1, tx2] = await MGMT.acquire(TOKEN) // TODO auto-acquire on init
       report(tx1.transactionHash)
       report(tx2.transactionHash)
     })
 
-    await task('point RPT account in schedule to RPT contract', async report => {
+    await deployTask('point RPT account in schedule to RPT contract', async report => {
       schedule.pools.filter(x=>x.name==='MintingPool')[0]
               .accounts.filter(x=>x.name==='RPT')[0]
               .address = RPT.address
@@ -145,86 +144,111 @@ export default async function demo ({network, agent, builder}) {
     })
 
     let launched
-    await task('launch the vesting', async report => {
+    await deployTask('launch the vesting', async report => {
       const {transactionHash, logs} = await MGMT.launch()
       launched = 1000 * Number(logs[0].events[1].attributes[1].value)
       report(transactionHash)
     })
 
-    await task.done()
+    await deployTask.done()
+
+    const runtimeTask = taskmaster({
+      header: [ 'time', 'info', 'time (msec)', 'gas (uSCRT)', 'overhead (msec)' ],
+      output: resolve(__dirname, 'artifacts', 'profile-runtime.md'),
+      agent
+    })
 
     let addedAccount = false
     let reallocated  = false
     while (true) {
-      await agent.nextBlock
-      const now = new Date()
-      const elapsed = now - launched
-      console.info(`\n⏱️  ${(elapsed/1000).toFixed(3)} "days" (seconds) after launch:`)
+      try {
+        await agent.nextBlock
 
-      const claimable = []
+        const now = new Date()
+        const elapsed = now - launched
+        console.info(`\n⏱️  ${Math.floor(elapsed/1000)} "days" (seconds) after launch:`)
 
-      for (const [name, recipient] of Object.entries(recipients)) {
-        if (name.startsWith('TokenPair')) {
-          // token pairs are only visible to the RPT contract
-          // so it doesn't make sense to pass them to the `unlocked` method
-          continue
-        }
-        const {progress} = await MGMT.progress(recipient.address, now)
-        console.info(
-          `${name}:`.padEnd(15),
-          progress.claimed.padStart(30), `/`, progress.unlocked.padStart(30)
-        )
-        // one random recipient with newly unlocked balance will claim:
-        if (name !== 'RPT' && progress.claimed < progress.unlocked) {
-          claimable.push(name)
-        }
-      }
+        const claimable = []
 
-      if (claimable.length > 0) {
-        const claimant = claimable[Math.floor(Math.random() * claimable.length)]
-        console.info(`\n${claimant} claims...`)
-        const recipient = recipients[claimant]
-        await MGMT.claim(recipient.agent)
-        const balance = String(await TOKEN.balance(recipient.agent, VK))
-        console.info(`balance of ${claimant} is now: ${balance}`)
-      }
-
-      if (!addedAccount && elapsed > 20000) {
-        console.log('\nadding new account to advisors pool...')
-        addedAccount = true
-        await MGMT.add('Advisors', {
-          name:     'NewAdvisor',
-          address:  recipients['NewAdvisor'].address,
-          amount:   "600000000000000000000",
-          cliff:    "100000000000000000000",
-          start_at: Math.floor(elapsed / 1000) + 5,
-          interval: 5,
-          duration: 25,
+        await runtimeTask('query vesting progress', async report => {
+          for (const [name, recipient] of Object.entries(recipients)) {
+            if (name.startsWith('TokenPair')) {
+              // token pairs are only visible to the RPT contract
+              // so it doesn't make sense to pass them to the `unlocked` method
+              continue
+            }
+            const {progress} = await MGMT.progress(recipient.address, now)
+            console.info(
+              `${name}:`.padEnd(15),
+              progress.claimed.padStart(30), `/`, progress.unlocked.padStart(30)
+            )
+            // one random recipient with newly unlocked balance will claim:
+            if (name !== 'RPT' && progress.claimed < progress.unlocked) {
+              claimable.push(name)
+            }
+          }
         })
-      }
 
-      if (!reallocated && elapsed > 30000) {
-        console.log('\nreallocating RPT...')
-        reallocated = true
-        await RPT.configure([
-          [recipients.TokenPair1.address,  "250000000000000000000"],
-          [recipients.TokenPair2.address, "1250000000000000000000"],
-          [recipients.TokenPair3.address, "1000000000000000000000"],
-        ])
-      }
-
-      console.debug('\nvesting RPT tokens...')
-      await RPT.vest()
-      for (const [name, recipient] of Object.entries(recipients)) {
-        if (name.startsWith('TokenPair')) {
-          console.log(
-            `${name}:`.padEnd(15),
-            String(await TOKEN.balance(recipient.agent, VK)).padStart(30)
-          )
+        if (claimable.length > 0) {
+          await runtimeTask('make one claim', async report => {
+            const claimant = claimable[Math.floor(Math.random() * claimable.length)]
+            console.info(`\n${claimant} claims...`)
+            const recipient = recipients[claimant]
+            await MGMT.claim(recipient.agent)
+            const balance = String(await TOKEN.balance(recipient.agent, VK))
+            console.info(`balance of ${claimant} is now: ${balance}`)
+          })
         }
-      }
 
+        if (!addedAccount && elapsed > 20000) {
+          await runtimeTask('add new account to advisors pool', async report => {
+            addedAccount = true
+            await MGMT.add('Advisors', {
+              name:     'NewAdvisor',
+              address:  recipients['NewAdvisor'].address,
+              amount:   "600000000000000000000",
+              cliff:    "100000000000000000000",
+              start_at: Math.floor(elapsed / 1000) + 5,
+              interval: 5,
+              duration: 25,
+            })
+          })
+        }
+
+        if (!reallocated && elapsed > 30000) {
+          await runtimeTask('reallocate RPT...', async report => {
+            reallocated = true
+            await RPT.configure([
+              [recipients.TokenPair1.address,  "250000000000000000000"],
+              [recipients.TokenPair2.address, "1250000000000000000000"],
+              [recipients.TokenPair3.address, "1000000000000000000000"],
+            ])
+          })
+        }
+
+        await runtimeTask('vest RPT tokens', async report => {
+          await RPT.vest()
+        })
+
+        await runtimeTask('query balances of RPT recipients', async report => {
+          for (const [name, recipient] of Object.entries(recipients)) {
+            if (name.startsWith('TokenPair')) {
+              console.log(
+                `${name}:`.padEnd(15),
+                String(await TOKEN.balance(recipient.agent, VK)).padStart(30)
+              )
+            }
+          }
+        })
+
+      } catch (e) {
+        console.info(`demo exited with error: ${e.message}`)
+        console.error(e)
+        break
+      }
     }
+
+    await runtimeTask.done()
   }
 
 }
