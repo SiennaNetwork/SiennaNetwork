c02044c720e2a522b3dad3f6bce65c70588f81b4
Milen Radkov <milenradkov@me.com>
2021-05-01 21:27:56 +0300

    feat(bridge): Add mainnet bridge address to deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,5 +3,7 @@ const WrappedSienna = artifacts.require('WrappedSienna');
 module.exports = function (deployer, networks, accounts) {
   // TODO: use real bridge address for e2e tests
   // deployer.deploy(WrappedSienna, accounts[0]);
-  deployer.deploy(WrappedSienna, '0xFA22c1BF3b076D2B5785A527C38949be47Ea1082');
+
+  // MAINNET SCRT_ETH BRIDGE
+  deployer.deploy(WrappedSienna, '0xf4b00c937b4ec4bb5ac051c3c719036c668a31ec');
 };
