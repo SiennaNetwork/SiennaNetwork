b8e6e2cb5baef024c6ac03bbab54c385cf58a334
IntegralTeam <repo@sfxdx.ru>
2021-06-03 21:47:43 +0300

    porting to u256 fix the overloding

--- contracts/libraries/src/core_library.rs
+++ contracts/libraries/src/core_library.rs
@@ -3,24 +3,31 @@ use crate::wad_ray_math;
 use super::wad_ray_math::WadRayMath;
 use cosmwasm_std::{CosmosMsg, Env, StdError, StdResult, Uint128};
 use std::ops::{Add, Div, Sub};
-
+use bigint::U256;
+use crate::wad_ray_math::WadRayMathU256;
+use lazy_static::lazy_static;
 enum InterestRateMode {
     Stable,
     Varible,
     None,
 }
-const SECODNS_PER_YEAR: u128 = 31_536_000;
+lazy_static! {
+    static ref SECODNS_PER_YEAR: U256 = {
+        U256::from(31_536_000)
+    };
+}
+//const SECODNS_PER_YEAR: U256 = U256::from(31_536_000);
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
 pub struct UserReserveData {
     //principal amount borrowed by the user.
-    principal_borrow_balance: u128,
+    principal_borrow_balance: U256,
     //cumulated variable borrow index for the user. Expressed in ray
-    last_variable_borrow_cumulative_index: u128,
+    last_variable_borrow_cumulative_index: U256,
     //origination fee cumulated by the user
-    origination_fee: u128,
+    origination_fee: U256,
     // stable borrow rate at which the user has borrowed. Expressed in ray
-    stable_borrow_rate: u128,
+    stable_borrow_rate: U256,
     last_update_timestamp: u64,
 
     //defines if a specific deposit should or not be used as a collateral in borrows
@@ -29,29 +36,29 @@ pub struct UserReserveData {
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
 pub struct ReserveData {
     //the liquidity index. Expressed in ray
-    last_liquidity_cumulate_index: u128,
+    last_liquidity_cumulate_index: U256,
     //the current supply rate. Expressed in ray
-    current_liquidity_rate: u128,
+    current_liquidity_rate: U256,
     //the total borrows of the reserve at a stable rate. Expressed in the currency decimals
-    total_borrow_stable: u128,
+    total_borrow_stable: U256,
     //the total borrows of the reserve at a variable rate. Expressed in the currency decimals
-    total_borrows_variable: u128,
+    total_borrows_variable: U256,
     //the current variable borrow rate. Expressed in ray
-    current_variable_borrow_rate: u128,
+    current_variable_borrow_rate: U256,
     //the current stable borrow rate. Expressed in ray
-    current_stable_borrow_rate: u128,
+    current_stable_borrow_rate: U256,
     //the current average stable borrow rate (weighted average of all the different stable rate loans). Expressed in ray
-    current_average_stable_borrow_rate: u128,
+    current_average_stable_borrow_rate: U256,
     //variable borrow index. Expressed in ray
-    last_variable_borrow_cumulative_index: u128,
+    last_variable_borrow_cumulative_index: U256,
     //the ltv of the reserve. Expressed in percentage (0-100)
-    base_ltv_as_collateral: u128,
+    base_ltv_as_collateral: U256,
     //the liquidation threshold of the reserve. Expressed in percentage (0-100)
-    liquidation_threshold: u128,
+    liquidation_threshold: U256,
     //the liquidation bonus of the reserve. Expressed in percentage
-    liquidation_bonus: u128,
+    liquidation_bonus: U256,
     //the decimals of the reserve asset
-    decimals: u128,
+    decimals: U256,
 
     /*
     address of the aToken representing the asset
@@ -83,7 +90,7 @@ impl ReserveData {
      * @param _reserve the reserve object
      * @return the normalized income. expressed in ray
      **/
-    pub fn get_normolized_income(&self, env: &Env) -> u128 {
+    pub fn get_normolized_income(&self, env: &Env) -> U256 {
         calculate_linear_interest(self.current_liquidity_rate, self.last_update_timestamp, env)
             .ray_mul(self.last_liquidity_cumulate_index)
     }
@@ -94,7 +101,7 @@ impl ReserveData {
      **/
     pub fn update_cumulative_indexes(&mut self, env: &Env) {
         let total_borrows = _get_total_borrows(&self);
-        if total_borrows > 0 {
+        if total_borrows > U256::from(0) {
             let camulated_liquidity_interest = calculate_linear_interest(
                 self.current_liquidity_rate,
                 self.last_update_timestamp,
@@ -119,10 +126,10 @@ impl ReserveData {
      * @param _totalLiquidity the total liquidity available in the reserve
      * @param _amount the amount to accomulate
      **/
-    pub fn cumulated_to_liquidity_index(&mut self, _total_liquidity: u128, _amount: u128) {
+    pub fn cumulated_to_liquidity_index(&mut self, _total_liquidity: U256, _amount: U256) {
         let amount_to_liquidity_ratio = _amount.wad_to_ray().ray_div(_total_liquidity.wad_to_ray());
 
-        let cumulated_liquidity = amount_to_liquidity_ratio.add(wad_ray_math::ray());
+        let cumulated_liquidity = amount_to_liquidity_ratio.add(wad_ray_math::ray_256());
 
         self.last_liquidity_cumulate_index =
             cumulated_liquidity.ray_mul(self.last_liquidity_cumulate_index);
@@ -138,7 +145,7 @@ impl ReserveData {
     pub fn init(
         &mut self,
         _a_token_address: String,
-        _decimals: u128,
+        _decimals: U256,
         _interested_rate_strategy_address: String,
     ) -> StdResult<()> {
         if !self.a_token_address.is_empty() {
@@ -147,12 +154,12 @@ impl ReserveData {
             ));
         }
 
-        if self.last_liquidity_cumulate_index == 0 {
-            self.last_liquidity_cumulate_index = wad_ray_math::ray();
+        if self.last_liquidity_cumulate_index == U256::from(0) {
+            self.last_liquidity_cumulate_index = wad_ray_math::ray_256();
         }
 
-        if self.last_variable_borrow_cumulative_index == 0 {
-            self.last_variable_borrow_cumulative_index = wad_ray_math::ray();
+        if self.last_variable_borrow_cumulative_index == U256::from(0) {
+            self.last_variable_borrow_cumulative_index = wad_ray_math::ray_256();
         }
         self.a_token_address = _a_token_address;
         self.decimals = _decimals;
@@ -164,7 +171,7 @@ impl ReserveData {
         Ok(())
     }
 
-    pub fn get_total_borrows(&self) -> u128 {
+    pub fn get_total_borrows(&self) -> U256 {
         _get_total_borrows(self)
     }
 
@@ -202,9 +209,9 @@ impl ReserveData {
 
     pub fn enable_as_collateral(
         &mut self,
-        _base_ltv_as_collateral: u128,
-        _liqudation_threshold: u128,
-        _liqudation_bouns: u128,
+        _base_ltv_as_collateral: U256,
+        _liqudation_threshold: U256,
+        _liqudation_bouns: U256,
     ) -> StdResult<()> {
         if self.borrowing_enabled == true {
             return Err(StdError::generic_err(
@@ -216,8 +223,8 @@ impl ReserveData {
         self.liquidation_threshold = _liqudation_threshold;
         self.liquidation_bonus = _liqudation_bouns;
 
-        if self.last_liquidity_cumulate_index == 0 {
-            self.last_liquidity_cumulate_index = wad_ray_math::ray();
+        if self.last_liquidity_cumulate_index == U256::from(0) {
+            self.last_liquidity_cumulate_index = wad_ray_math::ray_256();
         }
         Ok(())
     }
@@ -233,8 +240,8 @@ impl ReserveData {
 
     pub fn increase_total_borrows_stable_and_update_averege_rate(
         &mut self,
-        _amount: u128,
-        _rate: u128,
+        _amount: U256,
+        _rate: U256,
     ) {
         let previus_total_borrow_stable = self.total_borrow_stable;
         //updating reserve borrows stable
@@ -259,8 +266,8 @@ impl ReserveData {
      **/
     pub fn descrees_total_borrows_stable_and_update_average_rate(
         &mut self,
-        _amount: u128,
-        _rate: u128,
+        _amount: U256,
+        _rate: U256,
     ) -> StdResult<()> {
         if self.total_borrow_stable < _amount {
             return Err(StdError::generic_err("Invalid amount to decrease"));
@@ -270,8 +277,8 @@ impl ReserveData {
 
         //updating reserve borrows stable
         self.total_borrow_stable = self.total_borrow_stable.sub(_amount);
-        if self.total_borrow_stable == 0 {
-            self.current_average_stable_borrow_rate = 0;
+        if self.total_borrow_stable == U256::from(0) {
+            self.current_average_stable_borrow_rate = U256::from(0);
             return Ok(());
         }
 
@@ -298,7 +305,7 @@ impl ReserveData {
      * @param _reserve the reserve object
      * @param _amount the amount to add to the total borrows variable
      **/
-    pub fn increase_total_borrows_variable(&mut self, _amount: u128) {
+    pub fn increase_total_borrows_variable(&mut self, _amount: U256) {
         self.total_borrows_variable = self.total_borrows_variable.add(_amount);
     }
 
@@ -308,7 +315,7 @@ impl ReserveData {
      * @param _amount the amount to substract to the total borrows variable
      **/
 
-    pub fn decrease_total_borrows_varible(&mut self, _amount: u128) -> StdResult<()> {
+    pub fn decrease_total_borrows_varible(&mut self, _amount: U256) -> StdResult<()> {
         if self.total_borrows_variable < _amount {
             return Err(StdError::generic_err(
                 "The amount that is being subtracted from the variable total borrows is incorrect",
@@ -325,16 +332,16 @@ impl UserReserveData {
      * @param _reserve the reserve object
      * @return the user compounded borrow balance
      **/
-    pub fn get_compounded_borrow_balance(&self, _reserve: &ReserveData, env: &Env) -> u128 {
-        if self.principal_borrow_balance == 0 {
-            return 0;
+    pub fn get_compounded_borrow_balance(&self, _reserve: &ReserveData, env: &Env) -> U256 {
+        if self.principal_borrow_balance == U256::from(0) {
+            return U256::from(0);
         }
 
         let principal_borrow_balance_ray = self.principal_borrow_balance.wad_to_ray();
-        let mut compounded_balance = 0_u128;
-        let mut cumulated_interest = 0_u128;
+        let mut compounded_balance = U256::from(0);
+        let mut cumulated_interest = U256::from(0);
 
-        if self.stable_borrow_rate > 0 {
+        if self.stable_borrow_rate > U256::from(0) {
             cumulated_interest = calculate_compouned_interest(
                 self.stable_borrow_rate,
                 self.last_update_timestamp,
@@ -360,7 +367,7 @@ impl UserReserveData {
                 //no interest cumulation because of the rounding - we add 1 wei
                 //as symbolic cumulated interest to avoid interest free loans.
 
-                return self.principal_borrow_balance.add(1);
+                return self.principal_borrow_balance.add(U256::from(1));
             }
         }
         compounded_balance
@@ -374,24 +381,28 @@ impl UserReserveData {
 
 //pub fn
 
-pub fn calculate_linear_interest(_rate: u128, _last_update_timestamp: u64, env: &Env) -> u128 {
+pub fn calculate_linear_interest(_rate: U256, _last_update_timestamp: u64, env: &Env) -> U256 {
     //solim-disable-next-line
-    let time_defference = env.block.time.sub(_last_update_timestamp) as u128;
+    let time_defference = U256::from(env.block.time.sub(_last_update_timestamp));
 
     let time_delta = time_defference
         .wad_to_ray()
         .ray_div(SECODNS_PER_YEAR.wad_to_ray());
 
-    _rate.ray_mul(time_delta).add(wad_ray_math::ray())
+    _rate.ray_mul(time_delta).add(wad_ray_math::ray_256())
 }
 
-pub fn calculate_compouned_interest(_rate: u128, _last_update_timestamp: u64, env: &Env) -> u128 {
-    let time_defference = env.block.time.sub(_last_update_timestamp) as u128;
+pub fn calculate_compouned_interest(_rate: U256, _last_update_timestamp: u64, env: &Env) -> U256 {
+    let time_defference = U256::from(env.block.time.sub(_last_update_timestamp));
 
-    let rate_pre_second = _rate.div(SECODNS_PER_YEAR);
+    dbg!(time_defference);
+
+    let rate_pre_second = _rate.div(*SECODNS_PER_YEAR);
+
+    dbg!(rate_pre_second);
 
     rate_pre_second
-        .add(wad_ray_math::ray())
+        .add(wad_ray_math::ray_256())
         .ray_pow(time_defference)
 }
 /**
@@ -400,7 +411,7 @@ pub fn calculate_compouned_interest(_rate: u128, _last_update_timestamp: u64, en
  * @return the total borrows (stable + variable)
  **/
 
-fn _get_total_borrows(_reserve: &ReserveData) -> u128 {
+fn _get_total_borrows(_reserve: &ReserveData) -> U256 {
     _reserve
         .total_borrow_stable
         .add(_reserve.total_borrows_variable)
@@ -408,6 +419,10 @@ fn _get_total_borrows(_reserve: &ReserveData) -> u128 {
 
 #[cfg(test)]
 mod core_lib_tests {
+    use cosmwasm_std::{
+        testing::MOCK_CONTRACT_ADDR, BlockInfo, ContractInfo, HumanAddr, MessageInfo,
+    };
+
     use super::*;
 
     #[test]
@@ -415,7 +430,7 @@ mod core_lib_tests {
         let mut data = ReserveData::default();
         data.init(
             "deafult_address".to_string(),
-            1000,
+            U256::from(1000),
             "test_rate_strategy_address".to_string(),
         )
         .unwrap();
@@ -426,18 +441,66 @@ mod core_lib_tests {
         let mut data = ReserveData::default();
         data.init(
             "a_token_address".to_string(),
-            10,
+            U256::from(10),
             "interested_rate_strategy_address".to_string(),
-        ).unwrap();
-        let res =  data.init(
+        )
+        .unwrap();
+        let res = data.init(
             "a_token_address".to_string(),
-            10,
+            U256::from(10),
             "interested_rate_strategy_address".to_string(),
         );
         let want = Err(StdError::generic_err(
             "Reserve has already been initialized",
         ));
-        
-        assert_eq!(want,res);
+
+        assert_eq!(want, res);
+    }
+
+    #[test]
+    fn get_normolize_test() {
+        let data = ReserveData::default();
+
+        let normolze = data.get_normolized_income(&Env {
+            block: BlockInfo {
+                height: 100,
+                time: 1_571_797_420,
+                chain_id: "cosmos-testnet-14002".to_string(),
+            },
+            message: MessageInfo {
+                sender: HumanAddr("bob".to_string()),
+                sent_funds: vec![],
+            },
+            contract: ContractInfo {
+                address: HumanAddr::from(MOCK_CONTRACT_ADDR),
+            },
+            contract_key: Some("".to_string()),
+            contract_code_hash: "".to_string(),
+        });
+
+        println!("{:?}", normolze);
+    }
+
+    #[test]
+    fn calculate_linear_interest_test() {
+        let env = Env {
+            block: BlockInfo {
+                height: 100,
+                time: 1_571_800_420,
+                chain_id: "cosmos-testnet-14002".to_string(),
+            },
+            message: MessageInfo {
+                sender: HumanAddr("bob".to_string()),
+                sent_funds: vec![],
+            },
+            contract: ContractInfo {
+                address: HumanAddr::from(MOCK_CONTRACT_ADDR),
+            },
+            contract_key: Some("".to_string()),
+            contract_code_hash: "".to_string(),
+        };
+
+        let res = calculate_compouned_interest(U256::from(100000000), 1_571_800_320, &env);
+        println!("{:?}", res);
     }
 }
