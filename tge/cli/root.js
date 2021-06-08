3c044f52b51a221e8ef254d009eb9a31ff892d3c
Adam Avramov <adam@hack.bg>
2021-06-08 18:46:52 +0300

    chore: merge cli; workspace symlinks work

--- tge/cli/root.js
+++
@@ -1,5 +0,0 @@
-import { resolve, dirname, fileURLToPath } from '@hackbg/fadroma/js/sys.js'
-
-export const projectRoot = resolve(dirname(fileURLToPath(import.meta.url)), '..')
-
-export const abs = (...args) => resolve(projectRoot, ...args)
