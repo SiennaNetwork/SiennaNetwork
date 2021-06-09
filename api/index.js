d9938813f3e058ecc904a165a990a6a543a69c2b
Adam Avramov <adam@hack.bg>
2021-06-09 14:20:02 +0300

    chore: cleanup api

--- api/index.js
+++ api/index.js
@@ -2,16 +2,10 @@ import SNIP20Contract from './SNIP20.js'
 import MGMTContract from './MGMT.js'
 import RPTContract from './RPT.js'
 import RewardsContract from './Rewards.js'
-import WeightMasterContract from './WeightMaster.js'
-import LPStakingContract from './LPStaking.js'
-import GovernanceTokenContract from './GovernanceToken.js'
 
 export {
   SNIP20Contract,
   MGMTContract,
   RPTContract,
   RewardsContract,
-  WeightMasterContract,
-  LPStakingContract,
-  GovernanceTokenContract
 }
