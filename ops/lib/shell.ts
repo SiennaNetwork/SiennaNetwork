a3802f07828ef75778cf63053a6361a58d0b8cc4
Adam Avramov <adam@hack.bg>
2021-07-05 17:28:42 +0300

    fix(repl): safely display errors

--- ops/lib/shell.ts
+++ ops/lib/shell.ts
@@ -1,6 +1,8 @@
+import { abs } from './index.js'
 import { bold, render } from '@fadroma/utilities'
 import { SecretNetwork } from '@fadroma/scrt-agent'
 import * as repl from 'repl'
+import * as vm from 'vm'
 
 import {
   AMMContract,
@@ -45,12 +47,22 @@ export default async function shell (context = {}) {
     console.info('  '+Object.keys(Ensembles).map(x=>`Ensembles.${x}`).join('\n  '))
   }
   console.info(bold(`Other entities:`))
-  console.info('  '+['network', 'agent', 'builder'].join('\n  '))
+  console.info('  '+['network', 'agent', 'builder', 'workspace'].join('\n  '))
   const loop = repl.start({
     prompt: `${context.network}> `,
-    //writer: x => render(x)
+    //writer: x => render(x),
+    async eval (cmd, context, filename, callback) {
+      try {
+        const result = await Promise.resolve(vm.runInContext(cmd, context))
+        return callback(null, result)
+      } catch (e) {
+        console.error(e)
+        return callback()
+      }
+    },
   })
   await new Promise((resolve, reject)=>
-    loop.setupHistory('.fadroma_repl_history', (err, repl) => err ? reject(err) : resolve(repl)))
-  Object.assign(loop.context, { Contracts, Ensembles, network, agent, builder })
+    loop.setupHistory('.fadroma_repl_history',
+      (err, repl) => err ? reject(err) : resolve(repl)))
+  Object.assign(loop.context, { Contracts, Ensembles, network, agent, builder, workspace: abs() })
 }
