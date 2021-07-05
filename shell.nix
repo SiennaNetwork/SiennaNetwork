4dce8a87289b84d6cfacb7b8ccc6a34bfc5347b7
Adam Avramov <adam@hack.bg>
2021-07-05 14:36:08 +0300

    feat(shell): add rust toolchain selection to shell.nix

--- shell.nix
+++ shell.nix
@@ -4,16 +4,23 @@
     bash
     git
     jq
-    #binaryen
-    #cargo-tarpaulin
-    #cloc
+    binaryen
+    cargo-tarpaulin
+    cloc
     nodejs-14_x
-    #plantuml
-    #rustup
-    #wabt
+    plantuml
+    rustup
+    wabt
     yarn
   ];
   shellHook = ''
     export PS1='\n\e[0;35msɪᴇɴɴᴀ ⬢ \w\e[0m '
+    #rustup update
+    rustup target add wasm32-unknown-unknown
+    export RUST_BACKTRACE=1
+    rustup default nightly
+    export RUSTFLAGS="-Zmacro-backtrace"
+    rustup component add llvm-tools-preview
+    export PATH="$PATH:$HOME/.cargo/bin"
   '';
 }
