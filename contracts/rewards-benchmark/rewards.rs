ef712ea895a276941a6dd262e26149994dc24fea
Adam Avramov <adam@hack.bg>
2021-07-07 15:05:16 +0300

    fix(rewards-benchmark): make provided_token optional on init; don't forget to call save_state

--- contracts/rewards-benchmark/rewards.rs
+++ contracts/rewards-benchmark/rewards.rs
@@ -10,19 +10,22 @@ use sienna_reward_schedule::RewardPool;
 contract! {
 
     [State] {
-        provided_token: ContractInstance<CanonicalAddr>,
+        provided_token: Option<ContractInstance<CanonicalAddr>>,
         rewarded_token: ContractInstance<CanonicalAddr>,
         pool:           RewardPool
     }
 
     [Init] (deps, env, msg: {
-        provided_token: ContractInstance<HumanAddr>,
+        provided_token: Option<ContractInstance<HumanAddr>>,
         rewarded_token: ContractInstance<HumanAddr>
     }) {
         save_state!(State {
-            provided_token: provided_token.canonize(&deps.api)?,
+            provided_token: match provided_token {
+                None => None,
+                Some(provided_token) => Some(provided_token.canonize(&deps.api)?)
+            },
             rewarded_token: rewarded_token.canonize(&deps.api)?,
-            pool:           RewardPool::new(env.block.time)
+            pool: RewardPool::new(env.block.time)
         })
     }
 
@@ -36,52 +39,71 @@ contract! {
 
     [Handle] (deps, env, state, msg) -> Response {
 
+        SetProvidedToken (address: HumanAddr, code_hash: String) {
+            let address = deps.api.canonical_address(&address)?;
+            state.provided_token = Some(ContractInstance { address, code_hash });
+            save_state!();
+            Ok(HandleResponse::default())
+        }
+
         Lock (amount: Uint128) {
             let address = deps.api.canonical_address(&env.message.sender)?;
-            Ok(HandleResponse {
-                messages: vec![
-                    snip20::transfer_from_msg(
-                        env.message.sender,
-                        env.contract.address,
-                        state.pool.lock(env.block.time, address, amount),
-                        None,
-                        BLOCK_SIZE,
-                        state.provided_token.code_hash,
-                        deps.api.human_address(&state.provided_token.address)?
-                    )?
-                ],
-                log: vec![],
-                data: None
-            })
+            let locked = state.pool.lock(env.block.time, address, amount);
+            save_state!();
+            match state.provided_token {
+                Some(provided_token) => Ok(HandleResponse {
+                    messages: vec![
+                        snip20::transfer_from_msg(
+                            env.message.sender,
+                            env.contract.address,
+                            locked,
+                            None,
+                            BLOCK_SIZE,
+                            provided_token.code_hash,
+                            deps.api.human_address(&provided_token.address)?
+                        )?
+                    ],
+                    log: vec![],
+                    data: None
+                }),
+                None => panic!()
+            }
         }
 
         Retrieve (amount: Uint128) {
             let address = deps.api.canonical_address(&env.message.sender)?;
-            Ok(HandleResponse {
-                messages: vec![
-                    snip20::transfer_from_msg(
-                        env.contract.address,
-                        env.message.sender,
-                        state.pool.retrieve(env.block.time, address, amount)?,
-                        None,
-                        BLOCK_SIZE,
-                        state.provided_token.code_hash,
-                        deps.api.human_address(&state.provided_token.address)?
-                    )?
-                ],
-                log: vec![],
-                data: None
-            })
+            let retrieved = state.pool.retrieve(env.block.time, address, amount)?;
+            save_state!();
+            match state.provided_token {
+                Some(provided_token) => Ok(HandleResponse {
+                    messages: vec![
+                        snip20::transfer_from_msg(
+                            env.contract.address,
+                            env.message.sender,
+                            retrieved,
+                            None,
+                            BLOCK_SIZE,
+                            provided_token.code_hash,
+                            deps.api.human_address(&provided_token.address)?
+                        )?
+                    ],
+                    log: vec![],
+                    data: None
+                }),
+                None => panic!()
+            }
         }
 
         Claim () {
             let address = deps.api.canonical_address(&env.message.sender)?;
+            let claimed = state.pool.claim(address)?;
+            save_state!();
             Ok(HandleResponse {
                 messages: vec![
                     snip20::transfer_from_msg(
                         env.contract.address,
                         env.message.sender,
-                        state.pool.claim(address)?,
+                        claimed,
                         None,
                         BLOCK_SIZE,
                         state.rewarded_token.code_hash,
