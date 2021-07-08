dcf681ca3e43edd47d2a42306b3ca6e5ff3d0877
Adam Avramov <adam@hack.bg>
2021-07-08 20:30:21 +0300

    wip: test(rewards): factor out reusable part of test harness

--- contracts/rewards-benchmark/tests/harness.rs
+++ contracts/rewards-benchmark/tests/harness.rs
@@ -5,7 +5,7 @@ use fadroma::scrt::{
         Env, BlockInfo, MessageInfo, ContractInfo,
         Extern, MemoryStorage, Api, Querier,
         testing::{mock_dependencies, MockApi, MockQuerier},
-        from_binary,
+        from_binary, CosmosMsg, WasmMsg,
     },
     callback::{ContractInstance as ContractLink}
 };
@@ -13,13 +13,6 @@ use sienna_rewards_benchmark::{
     init, handle, query,
     msg::{Init, Handle as TX, Query as Q, Response}
 };
-
-// See https://docs.rs/cosmwasm-std/0.10.1/cosmwasm_std/testing/fn.mock_dependencies_with_balances.html
-const ADDR_LEN: usize = 45;
-
-pub struct Harness {
-    deps: Extern<MemoryStorage, MockApi, MockQuerier>
-}
 
 #[macro_export] macro_rules! assert_error {
     ($response:expr, $msg:expr) => {
@@ -30,20 +23,81 @@ pub struct Harness {
     }
 }
 
-impl Harness {
-    pub fn new () -> Self {
-        Self { deps: mock_dependencies(ADDR_LEN, &[]) }
-    }
+/// See https://docs.rs/cosmwasm-std/0.10.1/cosmwasm_std/testing/fn.mock_dependencies_with_balances.html
+const ADDR_LEN: usize = 45;
 
-    pub fn init (&mut self, height: u64, agent: &HumanAddr, msg: Init) -> StdResult<InitResponse> {
-        init(&mut self.deps, Env {
+/// Successful transaction return a vector of relevant messages and a count of any others
+type TxResult = StdResult<(Vec<CosmosMsg>, usize)>;
+
+pub struct RewardsHarness {
+    _deps: Extern<MemoryStorage, MockApi, MockQuerier>
+}
+
+pub trait Harness {
+
+    fn deps     (&self)     -> &Extern<MemoryStorage, MockApi, MockQuerier>;
+    fn deps_mut (&mut self) -> &mut Extern<MemoryStorage, MockApi, MockQuerier>;
+
+    fn init (&mut self, height: u64, agent: &HumanAddr, msg: Init) -> StdResult<InitResponse> {
+        init(self.deps_mut(), Env {
             block:    BlockInfo    { height, time: height * 5, chain_id: "secret".into() },
             message:  MessageInfo  { sender: agent.into(), sent_funds: vec![] },
-            contract: ContractInfo { address: "rewards_addr".into() },
-            contract_key:       Some("rewards_key".into()),
-            contract_code_hash: "rewards_hash".into()
+            contract: ContractInfo { address: "contract_addr".into() },
+            contract_key:       Some("contract_key".into()),
+            contract_code_hash: "contract_hash".into()
         }, msg)
     }
+
+    fn q (&self, q: Q) -> StdResult<Response> {
+        match query(self.deps(), q) {
+            Ok(response) => from_binary(&response),
+            Err(e) => Err(e)
+        }
+    }
+
+    fn tx (
+        &mut self, height: u64, agent: &HumanAddr, tx: TX
+    ) -> TxResult {
+        match handle(self.deps_mut(), Env {
+            block:    BlockInfo    { height, time: height * 5, chain_id: "secret".into() },
+            message:  MessageInfo  { sender: agent.into(), sent_funds: vec![] },
+            contract: ContractInfo { address: "contract_addr".into() },
+            contract_key:       Some("contract_key".into()),
+            contract_code_hash: "contract_hash".into()
+        }, tx) {
+            Ok(result) => {
+                let mut relevant = vec![];
+                let mut other     = 0;
+                for cosmos_msg in result.messages.iter() {
+                    match cosmos_msg {
+                        CosmosMsg::Wasm(wasm_msg) => match wasm_msg {
+                            WasmMsg::Execute { msg, .. } => relevant.push(from_binary(msg)?),
+                            _ => other += 1
+                        },
+                        _ => other += 1
+                    }
+                }
+                Ok((relevant, other))
+            },
+            Err(e) => Err(e)
+        }
+    }
+}
+
+impl Harness for RewardsHarness {
+    fn deps (&self) -> &Extern<MemoryStorage, MockApi, MockQuerier> {
+        &self._deps
+    }
+    fn deps_mut (&mut self) -> &mut Extern<MemoryStorage, MockApi, MockQuerier> {
+        &mut self._deps
+    }
+}
+
+impl RewardsHarness {
+    pub fn new () -> Self {
+        Self { _deps: mock_dependencies(ADDR_LEN, &[]) }
+    }
+
     pub fn init_configured (&mut self, height: u64, agent: &HumanAddr) -> StdResult<InitResponse> {
         self.init(height, agent, Init {
             provided_token: Some(ContractLink {
@@ -67,48 +121,26 @@ impl Harness {
             viewing_key: "".into()
         })
     }
-
-    pub fn q (&self, q: Q) -> StdResult<Response> {
-        from_binary(&query(&self.deps, q)?)
-    }
     pub fn q_status (&self, now: u64) -> StdResult<Response> {
         self.q(Q::Status { now })
-    }
-
-    pub fn tx (
-        &mut self, height: u64, agent: &HumanAddr, tx: TX
-    ) -> StdResult<HandleResponse> {
-        handle(&mut self.deps, Env {
-            block:    BlockInfo    { height, time: height * 5, chain_id: "secret".into() },
-            message:  MessageInfo  { sender: agent.into(), sent_funds: vec![] },
-            contract: ContractInfo { address: "rewards_addr".into() },
-            contract_key:       Some("rewards_key".into()),
-            contract_code_hash: "rewards_hash".into()
-        }, tx)
     }
 
     pub fn tx_set_token (
         &mut self, height: u64, agent: &HumanAddr,
         address: &str, code_hash: &str
-    ) -> StdResult<HandleResponse> {
+    ) -> TxResult {
         self.tx(height, agent, TX::SetProvidedToken {
             address:   HumanAddr::from(address),
             code_hash: code_hash.into(),
         })
     }
-    pub fn tx_lock (
-        &mut self, height: u64, agent: &HumanAddr, amount: u128
-    ) -> StdResult<HandleResponse> {
+    pub fn tx_lock (&mut self, height: u64, agent: &HumanAddr, amount: u128) -> TxResult {
         self.tx(height, agent, TX::Lock { amount: amount.into() })
     }
-    pub fn tx_retrieve (
-        &mut self, height: u64, agent: &HumanAddr, amount: u128
-    ) -> StdResult<HandleResponse> {
+    pub fn tx_retrieve (&mut self, height: u64, agent: &HumanAddr, amount: u128) -> TxResult {
         self.tx(height, agent, TX::Retrieve { amount: amount.into() })
     }
-    pub fn tx_claim (
-        &mut self, height: u64, agent: &HumanAddr
-    ) -> StdResult<HandleResponse> {
+    pub fn tx_claim (&mut self, height: u64, agent: &HumanAddr) -> TxResult {
         self.tx(height, agent, TX::Claim {})
     }
 }
--- contracts/rewards-benchmark/tests/rewards_test.rs
+++ contracts/rewards-benchmark/tests/rewards_test.rs
@@ -3,7 +3,7 @@
 #![allow(non_snake_case)]
 
 #[macro_use] extern crate kukumba;
-mod harness; use harness::Harness;
+mod harness; use harness::RewardsHarness;
 
 use fadroma::scrt::cosmwasm_std::{HumanAddr, StdError};
 
@@ -12,121 +12,123 @@ kukumba! {
 
     #[ok_init]
     given "no instance" {
-        let mut harness = Harness::new();
+        let mut test = RewardsHarness::new();
         let admin  = HumanAddr::from("admin");
     }
     when  "someone inits with an asset token address" {
-        let result = harness.init_configured(0, &admin)?;
+        let result = test.init_configured(0, &admin)?;
     }
     then  "the instance is ready"
     and   "it goes live when someone locks funds" {
-        assert_error!(harness.q_status(1u64), "missing data");
-        let result = harness.tx_lock(2, &admin, 1u128)?;
-        let result = harness.q_status(3u64)?;
+        assert_error!(test.q_status(1u64), "missing data");
+        let result = test.tx_lock(2, &admin, 1u128)?;
+        let result = test.q_status(3u64)?;
     }
 
     #[ok_init_then_provide]
     given  "no instance" {
-        let mut harness = Harness::new();
+        let mut test = RewardsHarness::new();
         let admin  = HumanAddr::from("admin");
         let badman = HumanAddr::from("badman");
     }
     when  "someone inits without providing an asset token address" {
-        let result = harness.init_partial(0, &admin)?;
+        let result = test.init_partial(0, &admin)?;
     }
     then  "the instance is not ready" {
-        assert_error!(harness.q_status(1u64), "not configured");
+        assert_error!(test.q_status(1u64), "not configured");
     }
     when  "a stranger tries to provide an asset token address" {
-        let result = harness.tx_set_token(2, &badman, "bad_addr", "bad_hash")?
+        let result = test.tx_set_token(2, &badman, "bad_addr", "bad_hash")?
     }
     then  "an error is returned and nothing changes" {
-        assert_error!(harness.q_status(3u64), "not configured");
+        assert_error!(test.q_status(3u64), "not configured");
     }
     when  "the admin provides an asset token address" {
-        let result = harness.tx_set_token(4, &admin, "ok_addr", "ok_hash")?
+        let result = test.tx_set_token(4, &admin, "ok_addr", "ok_hash")?
     }
     then  "the instance is ready"
     and   "it goes live when someone locks funds" {
-        assert_error!(harness.q_status(1u64), "missing data");
-        let result = harness.tx_lock(2, &admin, 1u128)?;
-        let result = harness.q_status(5u64)?;
+        assert_error!(test.q_status(1u64), "missing data");
+        let result = test.tx_lock(2, &admin, 1u128)?;
+        let result = test.q_status(5u64)?;
     }
 
     #[lock_and_retrieve]
     given "an instance" {
-        let mut harness = Harness::new();
+        let mut test = RewardsHarness::new();
         let admin   = HumanAddr::from("admin");
         let alice   = HumanAddr::from("alice");
         let bob     = HumanAddr::from("bob");
         let mallory = HumanAddr::from("mallory");
-        let result  = harness.init_configured(0, &admin)?;
+        let result  = test.init_configured(0, &admin)?;
     }
     when  "someone requests to lock tokens"
     then  "the instance transfers them to itself"
     and   "the liquidity provider starts accruing a reward" {
-        let result = harness.tx_lock(1, &alice, 100u128)?;
-        let result = harness.q_status(2u64)?;
+        let result = test.tx_lock(1, &alice, 100u128)?;
+        println!("{:?}", &result);
+        panic!();
+        let result = test.q_status(2u64)?;
     }
     when  "a provider requests to retrieve tokens"
     then  "the instance transfers them to the provider"
     and   "the reward now increases at a reduced rate" {
-        let result = harness.tx_retrieve(3, &alice, 50u128)?;
-        let result = harness.q_status(4u64.into())?;
+        let result = test.tx_retrieve(3, &alice, 50u128)?;
+        let result = test.q_status(4u64.into())?;
     }
     when  "a provider requests to retrieve all their tokens"
     then  "the instance transfers them to the provider"
     and   "their reward stops increasing" {
-        let result = harness.tx_retrieve(5, &alice, 50u128.into())?;
-        let result = harness.q_status(5u64)?;
+        let result = test.tx_retrieve(5, &alice, 50u128.into())?;
+        let result = test.q_status(5u64)?;
     }
     when  "someone else requests to lock tokens"
     then  "the previous provider's share of the rewards begins to diminish" {
-        let result = harness.tx_lock(6, &bob, 500)?;
-        let result = harness.q_status(7u64)?;
-        let result = harness.q_status(8u64)?;
+        let result = test.tx_lock(6, &bob, 500)?;
+        let result = test.q_status(7u64)?;
+        let result = test.q_status(8u64)?;
     }
     when  "a provider tries to retrieve too many tokens"
     then  "they get an error" {
         assert_error!(
-            harness.tx_retrieve(9, &bob, 1000u128),
+            test.tx_retrieve(9, &bob, 1000u128),
             "not enough balance (500 < 1000)"
         );
     }
     when  "a stranger tries to retrieve any tokens"
     then  "they get an error" {
         assert_error!(
-            harness.tx_retrieve(10, &mallory, 100u128),
+            test.tx_retrieve(10, &mallory, 100u128),
             "never provided liquidity"
         );
     }
 
     #[ok_claim]
     given "an instance" {
-        let mut harness = Harness::new();
+        let mut test = RewardsHarness::new();
         let admin   = HumanAddr::from("admin");
         let alice   = HumanAddr::from("alice");
-        let result  = harness.init_configured(0, &admin)?;
+        let result  = test.init_configured(0, &admin)?;
     }
     when  "a stranger tries to claim rewards"
     then  "they get an error" {
         assert_error!(
-            harness.tx_claim(1, &alice),
+            test.tx_claim(1, &alice),
             "never provided liquidity"
         );
     }
     when  "a provider claims their rewards"
     then  "the instance sends them reward tokens" {
-        let result = harness.tx_lock(2, &alice, 100)?;
-        let result = harness.tx_claim(3, &alice)?;
+        let result = test.tx_lock(2, &alice, 100)?;
+        let result = test.tx_claim(3, &alice)?;
     }
     when  "a provider claims their rewards twice"
     then  "they are sent only once" {
-        let result = harness.tx_claim(3, &alice)?;
+        let result = test.tx_claim(3, &alice)?;
     }
     when  "a provider claims their rewards later"
     then  "they receive an increment" {
-        let result = harness.tx_claim(4, &alice)?;
+        let result = test.tx_claim(4, &alice)?;
     }
 
     #[rewards_parallel_or_sequential]
