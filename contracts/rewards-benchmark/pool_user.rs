1ba77e965fc5f5247413c56ccc8002029a713e56
Adam Avramov <adam@hack.bg>
2021-07-16 10:48:32 +0300

    chore(rewards): remove old implementations
    was using up to 4 older versions of that code for reference

--- contracts/rewards-benchmark/pool_user.rs
+++
@@ -1,7 +0,0 @@
-use fadroma::scrt::{
-    cosmwasm_std::{Uint128, CanonicalAddr, StdResult, StdError, Storage, ReadonlyStorage},
-    storage::{Readonly, Writable},
-    utils::Uint256
-};
-
-macro_rules! error { ($info:expr) => { Err(StdError::generic_err($info)) }; }
