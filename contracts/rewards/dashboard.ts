a571fef9e74e254dc5bfe1c46adaa4436b4df513
Adam Avramov <adam@hack.bg>
2021-07-21 19:31:56 +0300

    wip: test(rewards): integration testing in browser

--- contracts/rewards/dashboard.ts
+++ contracts/rewards/dashboard.ts
@@ -1,25 +1,27 @@
 import './dashboard/style.css'
 import { Log, Table, PieChart, StackedPieChart } from './dashboard/widgets'
-import { T, Pool, User } from './dashboard/contract_mock'
+import { T, User } from './dashboard/contract_mock'
 import { random, pickRandom, throttle, after, addTo } from './dashboard/helpers'
 import initMock from './dashboard/contract_mock'
+import initReal from './dashboard/contract_real'
 
 // settings ----------------------------------------------------------------------------------------
 const UPDATE_INTERVAL = 0
 
 // dashboard components ----------------------------------------------------------------------------
-const log     = new Log()
-const table   = new Table()
-const current = new PieChart('Current amounts locked',  'locked')
-const stacked = new StackedPieChart()
-const ui = { log, table, current, stacked }
+const ui = {
+  log:     new Log(),
+  table:   new Table(),
+  current: new PieChart('Current amounts locked',  'locked'),
+  stacked: new StackedPieChart()
+}
 
 // the rewards contract and its participants -------------------------------------------------------
 const {pool, users} = initMock(ui)
-table.init(users)
+initReal(ui)
+ui.table.init(users)
 
 // start on click ----------------------------------------------------------------------------------
-
 document.body.onclick = () => {
   start()
   document.body.onclick = null
@@ -27,18 +29,15 @@ document.body.onclick = () => {
 
 function start () {
   // add components
-  for (const el of [ current
-                   , stacked
-                   //, lifetime
-                   //, earned
-                   //, claimed
-                   , log
-                   , table /*,sparkline*/]) {
-    addTo(document.body, el.root) }
+  for (const el of Object.values(ui)) {
+    addTo(document.body, el.root)
+  }
 
+  // add resize handler
   resize()
   window.addEventListener('resize', throttle(100, resize))
 
+  // start updating
   update()
   function update () {
     // advance time
@@ -67,19 +66,16 @@ function start () {
     }
 
     // update charts
-    for (const chart of [current
-                        ,stacked
-                        //,lifetime
-                        //,earned
-                        //,claimed
-                        ]) { chart.render() }
+    for (const chart of [ui.current,ui.stacked]) {
+      chart.render()
+    }
 
     // rinse and repeat
-    after(UPDATE_INTERVAL, update) }
+    after(UPDATE_INTERVAL, update)
+  }
+
   function resize () {
-    current.resize()
-    stacked.resize()
-    //lifetime.resize()
-    //earned.resize()
-    //claimed.resize()
-  } }
+    ui.current.resize()
+    ui.stacked.resize()
+  }
+}
--- contracts/rewards/dashboard/contract_real.ts
+++ contracts/rewards/dashboard/contract_real.ts
@@ -1,20 +1,49 @@
-import initRewards, { Contract as Rewards, Env, Init } from '../target/web/rewards.js'
+import { UIContext } from './contract_mock'
+import initRewards, {
+  Contract as Rewards,
+  Env,
+  Init, Q
+} from '../target/web/rewards.js'
 
-;(async () => {
-  const url      = new URL('rewards_bg.wasm', location.href)
-      , res      = await fetch(url.toString())
-      , buf      = await res.arrayBuffer()
-      , enc      = new TextEncoder()
+// converts from string to Utf8Array ---------------------------------------------------------------
+const enc = new TextEncoder()
 
+export default async function initReal (ui: UIContext) {
+
+  // wasm module load & init -----------------------------------------------------------------------
+  // thankfully wasm-pack/wasm-bindgen left an escape hatch
+  // because idk wtf is going on with the default loading code
+  const url = new URL('rewards_bg.wasm', location.href)
+      , res = await fetch(url.toString())
+      , buf = await res.arrayBuffer()
   await initRewards(buf)
+
+  // instantiate a reward pool ---------------------------------------------------------------------
   const rewards = new Rewards()
   const init = enc.encode(`{
-    "reward_token": {
-      "address": "",
-      "code_hash": ""
-    },
+    "reward_token": { "address": "", "code_hash": "" },
     "viewing_key": ""
   }`)
-  const init_result = rewards.init(new Env(), new Init(init))
+  const init_result = rewards.init(new Env(BigInt(0)), new Init(init))
   console.log({rewards, init_result})
-})()
+
+  const q_pool = rewards.query(new Q(enc.encode(`{
+    "pool_info": { "at": "0" }
+  }`)))
+
+  const q_user = rewards.query(new Q(enc.encode(`{
+    "user_info": { "at": "0", "address": "", "vk": "" }
+  }`)))
+
+  return {
+    pool: {
+      update () {
+        const TODO = 'TODO'
+        ui.log.now.textContent = `block ${TODO}`
+        ui.log.balance.textContent = `reward budget: ${TODO}`
+        ui.log.remaining.textContent = `${TODO} days remaining`
+      } 
+    },
+    users: {}
+  }
+}
