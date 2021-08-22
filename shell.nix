a1a2c1fb2ceeb0fd3ef0179ea6f080b625b314a8
Adam Avramov <adam@hack.bg>
2021-08-22 04:31:12 +0300

    chore: update dev shell

--- shell.nix
+++ shell.nix
@@ -1,27 +1,17 @@
-{pkgs?import<nixpkgs>{}}: pkgs.mkShell {
+{pkgs ? import <nixpkgs> { overlays = [
+  (import (builtins.fetchTarball {
+    url    = "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz";
+    sha256 = "1hpig8z4pzdwc2vazr6hg7qyxllbgznsaivaigjnmrdszlxz55zz";
+  }))];}}:
+pkgs.mkShell {
   name = "sienna";
   nativeBuildInputs = with pkgs; [
-    bash
-    git
-    jq
-    binaryen
-    cargo-tarpaulin
-    cloc
-    nodejs-14_x
-    plantuml
-    rustup
-    wabt
-    yarn
-    wasm-pack
-  ];
+    bash git jq cloc plantuml
+    nodejs-14_x yarn
+    (rustChannelOfTargets "nightly" "2021-08-04" ["wasm32-unknown-unknown"])
+    binaryen wabt wasm-pack wasm-bindgen-cli ];
   shellHook = ''
     export PS1='\n\e[0;35msɪᴇɴɴᴀ ⬢ \w\e[0m '
-    #rustup update
-    rustup target add wasm32-unknown-unknown
     export RUST_BACKTRACE=1
-    rustup default nightly
     export RUSTFLAGS="-Zmacro-backtrace"
-    rustup component add llvm-tools-preview
-    export PATH="$PATH:$HOME/.cargo/bin"
-  '';
-}
+    export PATH="$PATH:$HOME/.cargo/bin" ''; }
