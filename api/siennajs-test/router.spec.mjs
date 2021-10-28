d362a641ab8416c1aecb1df706222cb5a7fad353
Tibor Hudik <tibor@barrage.net>
2021-10-28 09:47:00 +0200

    Finalising hop assembler algorithm and its tests

--- api/siennajs-test/router.spec.mjs
+++ api/siennajs-test/router.spec.mjs
@@ -4,55 +4,54 @@ import { assert } from "chai";
 import siennajs from "../siennajs/index";
 
 const Assembler = siennajs.hop.Assembler;
-const get_path = siennajs.hop.get_path;
+const print_token_pair_comparable_tree = siennajs.hop.print_token_pair_comparable_tree;
 
 function create_token(id) {
   if (id) {
     return {
-      id: id,
-    //   token: {
-    //     Snip20Data: {
-    //       address: id,
-    //       code_hash: "",
-    //     },
-    //   },
-    };
+        Snip20Data: {
+          address: id,
+          code_hash: "",
+        },
+      };
   }
 
-  return {
-    id: "Scrt",
-    // token: { Scrt: {} },
-  };
+  return { Scrt: {} };
 }
 
 function create_pair(a, b) {
     return {
         A: a,
         B: b,
-        // pair_address: `${parseInt(Math.random() * 1000)}-address`,
-        // pair_code_hash: "",
+        pair_address: `${parseInt(Math.random() * 1000)}-address`,
+        pair_code_hash: "",
     };
 }
 
 describe("Test assembler in creating a route", function () {
   it("Can instantiate assembler with given tokens and pairs", async function () {
-      const A = create_token();
-      const B = create_token("token-3");
+      const A = create_token("token-2");
+      const B = create_token("token-4");
 
       const pairs = [
           create_pair(create_token(), create_token("token-2")),
           create_pair(create_token("token-2"), create_token("token-3")),
           create_pair(create_token("token-3"), create_token("token-4")),
           create_pair(create_token("token-2"), create_token("token-8")),
+          create_pair(create_token("token-18"), create_token("token-5")),
           create_pair(create_token("token-8"), create_token("token-18")),
           create_pair(create_token("token-18"), create_token("token-3")),
           create_pair(create_token(), create_token("token-4")),
       ];
 
       const assembler = new Assembler(pairs);
+      const tree = assembler.from(A).to(B).get_tree();
+      const hops = assembler.get();
+      const printout = print_token_pair_comparable_tree(tree);
 
-      console.log(JSON.stringify(assembler.get_hops(A, B), null, 2));
-      
-    //   console.log(get_path(pairs, A, B, 20000));
+    //   console.log(JSON.stringify(hops, null, 2));
+    //   console.log(printout.join(" ==> "));
+
+      assert.strictEqual(printout.join(" ==> "), "token-2 -> token-3 ==> token-3 -> token-4");
   });
 });
--- api/siennajs/lib/hop.ts
+++ api/siennajs/lib/hop.ts
@@ -1,5 +1,8 @@
-import { Address, TokenType, TokenPair } from "./core";
+import { Address } from "./core";
 
+/**
+ * Interface for any Snip20 token
+ */
 export interface Snip20Data {
     Snip20Data: {
         address: Address;
@@ -7,116 +10,374 @@ export interface Snip20Data {
     };
 }
 
+/**
+ * Interface for native token
+ */
 export interface Scrt {
     Scrt: {};
 }
 
+/**
+ * Enum of token representation used in the swap router contract
+ */
 export enum Token {
     Snip20Data,
     Scrt,
 }
 
+/**
+ * This is internal interface wrapper around the Token
+ * it helps us to compare them easily by using the id
+ */
 export interface TokenComparable {
     id: string | Address; // Here should be token address, or 'Scrt'
     token: Token;
 }
 
-export interface TokenPairExtended {
+/**
+ * Simple token pair interface for easier filling in the pairs
+ */
+export interface TokenPairSimple {
+    A: Token;
+    B: Token;
+    pair_address: Address;
+    pair_code_hash: string;
+}
+
+/**
+ * Token pair comparable that lets us compare tokens and run filter
+ * easily within Assembler class and its functions
+ */
+export interface TokenPairComparable {
     A: TokenComparable;
     B: TokenComparable;
     pair_address: Address;
     pair_code_hash: string;
-    hops?: TokenPairExtended[];
-    destinations?: string[];
 }
 
-export interface TokenPairTree {
+/**
+ * Token pair comparable tree that spans until its solved or broken.
+ * IMPORTANT: Only the last node will be marked as solved (performance fix),
+ * so to check if its solved you'll have to use the function provided in this file.
+ */
+export interface TokenPairComparableTree {
     A: TokenComparable;
     B: TokenComparable;
     pair_address: Address;
     pair_code_hash: string;
-    next?: TokenPairTree,
-    solved: boolean
+    next?: TokenPairComparableTree,
+    solved: boolean,
+    printout?: string[],
 }
 
+/**
+ * Hop representation used in the router contract,
+ * array of theese objects needs to be passed to the router
+ * in order for it to execute the transaction which will
+ * swap any for any token.
+ */
 export interface Hop {
     from_token: Token;
     pair_address: Address;
     pair_code_hash: string;
 }
 
-function map_destinations(pairs: TokenPairExtended[]): TokenPairExtended[] {
-    for (let i = 0; i < pairs.length; i++) {
-        pairs[i].destinations = pairs[i].destinations || [pairs[i].B.id];
+/**
+ * Helper for creating a comparable token pair from all its information
+ * 
+ * @param {Token} A 
+ * @param {Token} B 
+ * @param {Address} pair_address 
+ * @param {string} pair_code_hash 
+ * @returns {TokenPairComparable}
+ */
+export function into_token_pair_extended(A: Token, B: Token, pair_address: Address, pair_code_hash: string): TokenPairComparable {
+    return {
+        A: token_into_comparable(A),
+        B: token_into_comparable(B),
+        pair_address,
+        pair_code_hash,
+    };
+}
 
-        if (pairs[i].hops?.length) {
-            pairs[i].destinations = pairs[i].destinations?.concat(get_destinations(pairs[i].hops || []));
+/**
+ * Converts token enum into a comparable token
+ * 
+ * @param {Token} token 
+ * @returns {TokenComparable}
+ */
+export function token_into_comparable(token: Token): TokenComparable {
+    let id = "Scrt";
+
+    if (
+        token.hasOwnProperty("Snip20Data") &&
+        typeof (token as unknown as Snip20Data).Snip20Data === "object" &&
+        (token as unknown as Snip20Data).Snip20Data
+    ) {
+        id = (token as unknown as Snip20Data).Snip20Data.address;
+    }
+
+    return {
+        id,
+        token,
+    };
+}
+
+/**
+ * Converts simple token pair into a comparable token pair
+ * 
+ * @param {TokenPairSimple | TokenPairComparable} token_pair 
+ * @returns {TokenPairComparable}
+ */
+export function token_pair_simple_into_token_pair_comparable(token_pair: TokenPairSimple | TokenPairComparable): TokenPairComparable {
+    // @ts-ignore
+    if (typeof token_pair.A.id !== "undefined" && typeof token_pair.B.id !== "undefined") {
+        return token_pair as TokenPairComparable;
+    }
+
+    return {
+        ...token_pair,
+        A: token_into_comparable(token_pair.A as Token),
+        B: token_into_comparable(token_pair.B as Token),
+    }
+}
+
+/**
+ * Creates comparable token pair
+ * 
+ * @param {TokenPairComparable} pair 
+ * @returns {TokenPairComparableTree}
+ */
+export function token_pair_comparable_into_token_pair_comparable_tree(pair: TokenPairComparable): TokenPairComparableTree {
+    return {
+        ...pair,
+        solved: false,
+    };
+}
+
+/**
+ * Converts the single comparable token pair into a hop
+ * 
+ * @param {TokenPairComparable} pair 
+ * @returns {Hop}
+ */
+export function token_pair_comparable_into_hop(pair: TokenPairComparable): Hop {
+    return {
+        from_token: pair.A.token,
+        pair_address: pair.pair_address,
+        pair_code_hash: pair.pair_code_hash,
+    }
+}
+
+/**
+ * Converts solved tree into an array of hops that can be then used for the swap router
+ * 
+ * @param {TokenPairComparableTree} pair 
+ * @returns {Hop[]}
+ */
+export function token_pair_tree_into_hops(pair: TokenPairComparableTree): Hop[] {
+    if (!pair.solved) {
+        throw new Error("Token path is not solved, cannot create hops");
+    }
+
+    const hops = [];
+    let run = true;
+    let node = pair;
+
+    hops.push(token_pair_comparable_into_hop(node));
+
+    while (run === true) {
+        if (typeof node.next !== "undefined") {
+            hops.push(token_pair_comparable_into_hop(node.next));
+            node = node.next;
+        }
+        else {
+            run = false;
         }
     }
 
-    return pairs;
+    return hops;
 }
 
-function get_destinations(pairs: TokenPairExtended[]): string[] {
-    let destinations: string[] = [];
+/**
+ * Check if the tree is considered solved (if the last node is solved)
+ * 
+ * @param {TokenPairComparableTree} tree 
+ * @returns {boolean}
+ */
+export function token_pair_comparable_tree_chain_solved(tree: TokenPairComparableTree): boolean {
+    let node: TokenPairComparableTree | undefined = tree;
+    let first = true;
 
-    for (let i = 0; i < pairs.length; i++) {
-        pairs[i].destinations = pairs[i].destinations || [pairs[i].B.id];
-
-        if (pairs[i].hops?.length) {
-            pairs[i].destinations = pairs[i].destinations?.concat(get_destinations(pairs[i].hops || []));
+    while (node) {
+        // IMPORTANT: This will add the constraint that is presented
+        // in the router contract: Scrt can only be first or last part
+        // of the hop chain. It cannot appear anywhere in the middle
+        // of the chain.
+        if (!first && node.A.id === "Scrt") {
+            return false;
         }
 
-        destinations = destinations.concat(pairs[i].destinations || []);
+        first = false;
+
+        if (node?.solved) {
+            return true;
+        }
+
+        node = node.next;
     }
 
-    return destinations;
+    return false;
 }
 
+/**
+ * Returns an array that has each step printed in human-readeable format
+ * 
+ * @param {TokenPairComparableTree} tree 
+ * @returns {string[]}
+ */
+export function print_token_pair_comparable_tree(tree: TokenPairComparableTree): string[] {
+    let node: TokenPairComparableTree | undefined = tree;
+    let printout: string[] = [];
+
+    while (node) {
+        printout.push(`${node.A.id} -> ${node.B.id}`);
+        node = node.next;
+    }
+
+    return printout;
+}
+
+/**
+ * This class will essentially do all the work for generating the path of exchange
+ * @class {Assembler}
+ */
 export class Assembler {
-    private pairs: TokenPairExtended[] = [];
+    private A?: TokenComparable;
+    private B?: TokenComparable;
+    private pairs: TokenPairComparableTree[] = [];
+    private parents: TokenPairComparableTree[] = [];
 
-    constructor(pairs: TokenPairExtended[]) {
-        const secondary: TokenPairExtended[] = [];
-
+    constructor(pairs: TokenPairSimple[] | TokenPairComparable[], private filter: string[] = []) {
         for (const pair of pairs) {
-            secondary.push({
-                ...pair,
-                B: pair.A,
-                A: pair.B,
+            let pair_comparable = token_pair_comparable_into_token_pair_comparable_tree(
+                token_pair_simple_into_token_pair_comparable(pair)
+            );
+
+            this.pairs.push(pair_comparable);
+            this.pairs.push({
+                ...pair_comparable,
+                A: pair_comparable.B,
+                B: pair_comparable.A,
             });
         }
-
-        pairs = pairs.concat(secondary);
-
-        this.pairs = pairs;
     }
 
-    get_hops(A: TokenComparable, B: TokenComparable): TokenPairExtended[] {
-        if (A.id === B.id) {
-            throw new Error("Token A and token B must be different");
-        }
-        
-        const paths = this.pairs.filter(p => p.A.id === A.id);
+    /**
+     * Set token that we want to swap
+     * 
+     * @param {Token} token 
+     * @returns {Assembler}
+     */
+    from(token: Token): this {
+        this.A = token_into_comparable(token);
 
-        for (const path of paths) {
-            const filtered = this.pairs.filter(p => p.A.id !== path.A.id && p.B.id !== path.B.id);
-
-            path.hops = this.nest(path, filtered);
-        }
-
-        return map_destinations(paths)
-            .filter(p => p.destinations?.includes(B.id))
-            .sort((a, b) => (a.destinations || []).length - (b.destinations || []).length);
+        return this;
     }
 
-    private nest(parent: TokenPairExtended, pairs: TokenPairExtended[]): TokenPairExtended[] {
-        const hops = pairs.filter(child => parent.B.id === child.A.id);
+    /**
+     * Set token that we want to get after the swap
+     * 
+     * @param {Token} token 
+     * @returns {Assembler}
+     */
+    to(token: Token): this {
+        if (!this.A) {
+            throw new Error("You'll have to provide the 'from' token first");
+        }
 
-        return hops
-            .map(hop => ({
-                ...hop,
-                hops: this.nest(hop, pairs.filter(p => p.A.id !== hop.A.id && p.B.id !== hop.B.id)),
-            }));
+        this.B = token_into_comparable(token);
+
+        return this.prepare();
+    }
+
+    /**
+     * Prepare this class so that everything is validated and all the 
+     * parents and left over pairs are set.
+     * 
+     * @returns {Assembler}
+     */
+    private prepare(): this {
+        if ((!this.A || !this.B) || (this.A?.id === this.B?.id)) {
+            throw new Error("Invalid pair provided");
+        }
+
+        // Parents can be any of the pairs that have A as A
+        this.parents = this.pairs.filter(p => {
+            return p.A.id === this.A?.id;
+        }).map(p => token_pair_comparable_into_token_pair_comparable_tree(p));
+
+        this.filter.push(this.A?.id);
+
+        // Left over pairs cannot have A in either A or B
+        this.pairs = this.pairs.filter(p => {
+            return !this.filter.includes(p.A.id) && !this.filter.includes(p.B.id);
+        });
+
+        return this;
+    }
+
+    /**
+     * Create token tree that will let us know what paths we need to take in order
+     * to swap A for B
+     * 
+     * @returns {TokenPairComparableTree}
+     */
+    get_tree(): TokenPairComparableTree {
+        if ((!this.A || !this.B) || (this.A?.id === this.B?.id)) {
+            throw new Error("Invalid pair provided");
+        }
+
+        // First, we will try to find a perfect match with a single pair
+        for (const parent of this.parents) {
+            if (parent.A.id === this.A?.id && parent.B.id === this.B?.id) {
+                return { ...token_pair_comparable_into_token_pair_comparable_tree(parent), solved: true };
+            }
+        }
+
+        // Try to finish each parents tree
+        for (let i = 0; i < this.parents.length; i++) {
+            try {
+                const assembler = (new Assembler(this.pairs, this.filter)).from(this.parents[i].B.token).to(this.B.token);
+
+                this.filter = assembler.filter;
+
+                this.parents[i].next = assembler.get_tree();
+
+                this.parents[i].solved = token_pair_comparable_tree_chain_solved(this.parents[i]);
+            }
+            catch (e) { }
+        }
+
+        // Filter parents to only have the solved ones left and then sort is so that the 
+        // first in line is the one with the least amount of hops in the chain
+        this.parents = this.parents
+            .filter(p => p.solved)
+            .sort((a, b) => print_token_pair_comparable_tree(a).length - print_token_pair_comparable_tree(b).length);
+
+        if (!this.parents.length) {
+            throw new Error("No possible solution for given pair");
+        }
+
+        return this.parents[0];
+    }
+
+    /**
+     * Return the list of all the hops needed to execute the swap
+     * 
+     * @returns {Hop[]}
+     */
+    get(): Hop[] {
+        return token_pair_tree_into_hops(this.get_tree());
     }
 }
