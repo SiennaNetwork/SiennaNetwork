1ed7faaea7afa2329e266a22f49ce8b66a1edae9
Tibor Hudik <tibor@barrage.net>
2021-11-22 09:55:01 +0100

    WIP: router siennajs handler

---
+++ api/siennajs/lib/router.ts
@@ -0,0 +1,89 @@
+import {
+    Address, Uint128, Fee, create_fee,
+    get_token_type, TypeOfToken, CustomToken, TokenTypeAmount, Decimal, add_native_balance
+} from './core'
+import { SmartContract, Querier, Executor } from './contract'
+import { Snip20Contract } from './snip20'
+
+import { SigningCosmWasmClient, ExecuteResult } from 'secretjs'
+import { Hop } from './hop'
+import { ExchangeContract } from './exchange'
+
+export class RouterContract extends SmartContract<RouterExecutor, RouterQuerier> {
+    exec(fee?: Fee, memo?: string): RouterExecutor {
+        return new RouterExecutor(
+            this.address,
+            () => this.query.apply(this),
+            this.execute_client,
+            fee,
+            memo
+        )
+    }
+    
+    query(): RouterQuerier {
+        return new RouterQuerier(this.address, this.query_client)
+    }
+}
+
+export class RouterExecutor extends Executor {
+    constructor(
+        address: Address,
+        private querier: () => RouterQuerier,
+        client?: SigningCosmWasmClient,
+        fee?: Fee,
+        memo?: string,
+    ) {
+        super(address, client, fee, memo)
+    }
+
+    async swap(
+        hops: Hop[],
+        amount: Uint128,
+        recipient?: Address | null,
+        expected_return?: Decimal | null,
+    ): Promise<ExecuteResult> {
+        if (hops.length === 0) {
+            throw new Error("You have to provide hops for the swap to happen")
+        }
+
+        const first_hop = hops[0]
+
+        if (hops.length === 1) {
+            return (new ExchangeContract(first_hop.pair_address, this.client, this.client))
+                .exec()
+                .swap(new TokenTypeAmount(first_hop.from_token, amount), recipient, expected_return)
+        }
+
+        if (get_token_type(first_hop.from_token) == TypeOfToken.Native) {
+            const msg = {
+                hops,
+                to: recipient,
+                expected_return,
+            }
+
+            const transfer = add_native_balance(new TokenTypeAmount(first_hop.from_token, amount))
+            return this.run(msg, '280000', transfer)
+        }
+
+        const msg = {
+            hops,
+            to: recipient,
+            expected_return,
+        }
+
+        const fee = this.fee || create_fee('530000')
+
+        const token_addr = (first_hop.from_token as CustomToken).custom_token.contract_addr;
+        const snip20 = new Snip20Contract(token_addr, this.client)
+
+        return snip20.exec(fee, this.memo).send(this.address, amount, msg)
+    }
+}
+
+export class RouterQuerier extends Querier {
+    async supported_tokens(): Promise<Address[]> {
+        const result = await this.run({ supported_tokens: {} }) as Address[];
+        
+        return result;        
+    }
+}
