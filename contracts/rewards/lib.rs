b191fad7adc54aa1ffc68222457674ac762f9b01
Adam Avramov <adam.b.avramov@gmail.com>
2021-11-23 10:36:32 +0100

    fix(rewards): add chain entrypoints

--- contracts/rewards/lib.rs
+++ contracts/rewards/lib.rs
@@ -1,4 +1,4 @@
-#[macro_use] extern crate fadroma;
+extern crate fadroma;
 pub use fadroma::*;
 
 pub mod algo; use crate::algo::{*, RewardsResponse};
@@ -12,7 +12,17 @@ pub mod migration; use crate::migration::*;
 #[cfg(test)] mod test;
 
 #[cfg(browser)] #[macro_use] extern crate wasm_bindgen;
-#[cfg(all(feature="browser",target_arch="wasm32"))] mod wasm { fadroma::bind_js!(super); }
+#[cfg(all(feature="browser",target_arch="wasm32"))] mod wasm { fadroma_bind_js::bind_js!(fadroma, super); }
+#[cfg(all(not(feature="browser"),target_arch="wasm32"))] mod wasm {
+    use super::{init, handle, query};
+    use cosmwasm_std::{do_handle, do_init, do_query, ExternalApi, ExternalQuerier, ExternalStorage};
+    #[no_mangle] extern "C" fn init(env_ptr: u32, msg_ptr: u32) -> u32 {
+        do_init(&init::<ExternalStorage, ExternalApi, ExternalQuerier>, env_ptr, msg_ptr) }
+    #[no_mangle] extern "C" fn handle(env_ptr: u32, msg_ptr: u32) -> u32 {
+        do_handle(&handle::<ExternalStorage, ExternalApi, ExternalQuerier>, env_ptr, msg_ptr) }
+    #[no_mangle] extern "C" fn query(msg_ptr: u32) -> u32 {
+        do_query(&query::<ExternalStorage, ExternalApi, ExternalQuerier>, msg_ptr) }
+}
 
 pub fn init <S: Storage, A: Api, Q: Querier> (deps: &mut Extern<S, A, Q>, env: Env, msg: Init)
     -> StdResult<InitResponse>   { Contract::init(deps, env, msg) }
