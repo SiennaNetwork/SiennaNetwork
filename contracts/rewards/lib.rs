698a9637cdbc1b0cb7ee178698948c8c31a9d90e
Adam A <adam@hack.bg>
2021-11-30 13:10:40 +0200

    bench(rewards): benchmark build passes

--- contracts/rewards/lib.rs
+++ contracts/rewards/lib.rs
@@ -14,14 +14,16 @@ pub mod migration; use crate::migration::*;
 #[cfg(browser)] #[macro_use] extern crate wasm_bindgen;
 #[cfg(all(feature="browser",target_arch="wasm32"))] mod wasm { fadroma_bind_js::bind_js!(fadroma, super); }
 #[cfg(all(not(feature="browser"),target_arch="wasm32"))] mod wasm {
-    use super::{init, handle, query};
-    use cosmwasm_std::{do_handle, do_init, do_query, ExternalApi, ExternalQuerier, ExternalStorage};
+    use fadroma::{do_handle, do_init, do_query, ExternalApi, ExternalQuerier, ExternalStorage};
     #[no_mangle] extern "C" fn init(env_ptr: u32, msg_ptr: u32) -> u32 {
-        do_init(&init::<ExternalStorage, ExternalApi, ExternalQuerier>, env_ptr, msg_ptr) }
+        do_init(&super::init::<ExternalStorage, ExternalApi, ExternalQuerier>, env_ptr, msg_ptr)
+    }
     #[no_mangle] extern "C" fn handle(env_ptr: u32, msg_ptr: u32) -> u32 {
-        do_handle(&handle::<ExternalStorage, ExternalApi, ExternalQuerier>, env_ptr, msg_ptr) }
+        do_handle(&super::handle::<ExternalStorage, ExternalApi, ExternalQuerier>, env_ptr, msg_ptr)
+    }
     #[no_mangle] extern "C" fn query(msg_ptr: u32) -> u32 {
-        do_query(&query::<ExternalStorage, ExternalApi, ExternalQuerier>, msg_ptr) }
+        do_query(&super::query::<ExternalStorage, ExternalApi, ExternalQuerier>, msg_ptr)
+    }
 }
 
 pub fn init <S: Storage, A: Api, Q: Querier> (deps: &mut Extern<S, A, Q>, env: Env, msg: Init)
