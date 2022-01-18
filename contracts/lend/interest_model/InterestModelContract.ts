868f02620d3f9a707899d2955f53bc9092da30e9
Adam A <adam@hack.bg>
2022-01-19 00:53:12 +0200

    cleanup: remove more redundant imports

--- contracts/lend/interest_model/InterestModelContract.ts
+++ contracts/lend/interest_model/InterestModelContract.ts
@@ -1,7 +1,5 @@
 import { ScrtContract_1_2 } from "@fadroma/scrt"
 
-import { workspace } from '@sienna/settings'
-
 import { InitMsg } from './schema/init_msg.d'
 
 export class InterestModelContract extends ScrtContract_1_2 {
