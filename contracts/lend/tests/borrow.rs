c1c83ff579bf7dcfbb65071ecdbad132674c9199
aakamenov <asparuh@hack.bg>
2022-01-27 17:58:28 +0200

    Add repay tests.

--- contracts/lend/tests/borrow.rs
+++ contracts/lend/tests/borrow.rs
@@ -2,7 +2,8 @@ use lend_shared::{
     fadroma::{
         ensemble::MockEnv,
         permit::Permit,
-        cosmwasm_std::{Uint128, HumanAddr, StdError, Binary},
+        cosmwasm_std::{Uint128, HumanAddr, StdError, Binary, to_binary},
+        snip20_impl::msg as snip20,
         Decimal256, Uint256
     },
     interfaces::{
@@ -33,6 +34,25 @@ impl Lend {
                     "balance"
                 ).into(),
                 block: None
+            }
+        ).unwrap()
+    }
+
+    #[inline]
+    pub fn id(
+        &self,
+        address: impl Into<HumanAddr>,
+        market: HumanAddr
+    ) -> Binary {
+        self.ensemble.query(
+            market.clone(),
+            market::QueryMsg::Id {
+                method: Permit::new(
+                    address,
+                    vec![ market::MarketPermissions::Id ],
+                    vec![ market ],
+                    "id"
+                ).into()
             }
         ).unwrap()
     }
@@ -128,6 +148,9 @@ fn borrow() {
         market::QueryMsg::State { block: None }
     ).unwrap();
 
+    assert_eq!(state.total_borrows, 100.into());
+    assert_eq!(state.total_supply, 750.into());
+
     let utilization_rate = utilization_rate(
         Decimal256::from_uint256(state.underlying_balance).unwrap(),
         Decimal256::from_uint256(state.total_borrows).unwrap(),
@@ -159,17 +182,105 @@ fn borrow() {
     assert_eq!(chester.info.principal, borrow_amount.into());
     assert_eq!(chester.info.interest_index, state.borrow_index);
 
-    let id: Binary = lend.ensemble.query(
+    let id = lend.id(CHESTER, market_one.address);
+    assert_eq!(chester.id, id);
+}
+
+#[test]
+fn self_repay() {
+    do_repay(true)
+}
+
+#[test]
+fn repay() {
+    do_repay(false)
+}
+
+fn do_repay(self_repay: bool) {
+    let mut lend = Lend::default();
+
+    let underlying_1 = lend.new_underlying_token("SSCRT", 6).unwrap();
+    let underlying_2 = lend.new_underlying_token("SIENNA", 18).unwrap();
+
+    let market_one = lend.whitelist_market(
+        underlying_1.clone(),
+        Decimal256::percent(90),
+        None
+    )
+    .unwrap()
+    .contract;
+
+    let market_two = lend.whitelist_market(
+        underlying_2,
+        Decimal256::percent(90),
+        None
+    )
+    .unwrap()
+    .contract;
+
+    let prefund_amount = Uint128(500);
+    let borrow_amount = Uint128(100);
+
+    lend.prefund_and_deposit(BOB, prefund_amount, market_one.address.clone());
+    lend.prefund_and_deposit(ALICE, prefund_amount, market_one.address.clone());
+    lend.prefund_and_deposit(CHESTER, Uint128(112), market_two.address.clone());
+
+    lend.ensemble.execute(
+        &overseer::HandleMsg::Enter {
+            markets: vec![
+                market_one.address.clone(),
+                market_two.address.clone()
+            ]
+        },
+        MockEnv::new(CHESTER, lend.overseer.clone())
+    ).unwrap();
+
+    lend.ensemble.execute(
+        &market::HandleMsg::Borrow {
+            amount: borrow_amount.into()
+        },
+        MockEnv::new(CHESTER, market_one.clone())
+    ).unwrap();
+
+    let (repayer, id) = if self_repay {
+        (CHESTER, None)
+    } else {
+        (ALICE, Some(lend.id(CHESTER, market_one.address.clone())))
+    };
+
+    lend.prefund_user(repayer, borrow_amount, underlying_1.clone());
+
+    lend.ensemble.execute(
+        &snip20::HandleMsg::Send {
+            recipient: market_one.address.clone(),
+            recipient_code_hash: None,
+            amount: borrow_amount,
+            msg: Some(to_binary(&market::ReceiverCallbackMsg::Repay {
+                borrower: id
+            }).unwrap()),
+            memo: None,
+            padding: None
+        },
+        MockEnv::new(repayer, underlying_1)
+    ).unwrap();
+
+    let state: market::State = lend.ensemble.query(
         market_one.address.clone(),
-        market::QueryMsg::Id {
-            method: Permit::new(
-                CHESTER,
-                vec![ market::MarketPermissions::Id ],
-                vec![ market_one.address ],
-                "id"
-            ).into()
+        market::QueryMsg::State { block: None }
+    ).unwrap();
+
+    assert_eq!(state.total_borrows, Uint256::zero());
+    assert_eq!(state.total_supply, 750.into());
+
+    let info: Vec<market::Borrower> = lend.ensemble.query(
+        market_one.address.clone(),
+        market::QueryMsg::Borrowers {
+            limit: None,
+            start_after: None,
+            block: state.accrual_block
         }
     ).unwrap();
 
-    assert_eq!(chester.id, id);
+    // Borrows repaid in full are removed from storage.
+    assert_eq!(info.len(), 0);
 }
