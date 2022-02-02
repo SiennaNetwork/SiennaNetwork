0dfd8481d754e88cda1e0196dc38e9867d1e2f44
denismaxim0v <denis_maxim0v@protonmail.com>
2022-02-02 17:25:49 +0200

    wip: deploy script for lend

--- contracts/lend/deploy.ts
+++ contracts/lend/deploy.ts
@@ -19,10 +19,6 @@ import {
 } from "@sienna/api";
 
 import settings, { workspace } from "@sienna/settings";
-import {
-  ContractInstantiationInfo,
-  ContractInfo,
-} from "../../frontends/siennajs/lib/core";
 
 const console = Console("@sienna/amm/upgrade");
 
@@ -36,9 +32,9 @@ export async function deployLend({
   deployment: Deployment;
   prefix: string;
   OVERSEER: LendOverseerContract;
+  INTEREST_MODEL: InterestModelContract;
 }> {
-  console.info(bold("Admin balance: "), await admin.balance);
-
+  let mock_oracle = { address: null, codeHash: null };
   const [INTEREST_MODEL, ORACLE, MARKET, OVERSEER, MOCK_ORACLE] =
     await chain.buildAndUpload(admin, [
       new InterestModelContract({ workspace }),
@@ -48,7 +44,8 @@ export async function deployLend({
       new MockOracleContract({ workspace }),
     ]);
 
-  let interestModel = await deployment.createContract(admin, INTEREST_MODEL, {
+  // paramters taken from Compound
+  await deployment.createContract(admin, INTEREST_MODEL, {
     base_rate_year: "20000000000000000",
     blocks_year: 6311520,
     jump_multiplier_year: "200000000000000000",
@@ -56,16 +53,28 @@ export async function deployLend({
     multiplier_year: "200000000000000000",
   });
 
-  let mock_oracle = await deployment.createContract(admin, MOCK_ORACLE, {});
+  if (chain.isLocalnet) {
+    let contract = await deployment.createContract(admin, MOCK_ORACLE, {});
+
+    mock_oracle.address = contract.initTx.contractAddress;
+    mock_oracle.codeHash = contract.codeHash;
+  }
 
   let overseer = await deployment.createContract(admin, OVERSEER, {
     close_factor: "500000000000000000",
     entropy: randomHex(36),
-    market_contract: { code_hash: MARKET.codeHash, id: MARKET.codeId },
-    oracle_contract: { code_hash: ORACLE.codeHash, id: ORACLE.codeId },
+    market_contract: {
+      code_hash: MARKET.codeHash,
+      id: MARKET.codeId,
+    },
+    oracle_contract: {
+      code_hash: MARKET.codeHash,
+      id: MARKET.codeId,
+    },
+    // TODO: add band oracle address and hash
     oracle_source: {
-      address: mock_oracle.initTx.contractAddress,
-      code_hash: mock_oracle.codeHash,
+      address: chain.isLocalnet ? mock_oracle.address : "",
+      code_hash: chain.isLocalnet ? mock_oracle.codeHash : "",
     },
     premium: "1080000000000000000",
     prng_seed: randomHex(36),
@@ -76,5 +85,6 @@ export async function deployLend({
     deployment,
     prefix,
     OVERSEER,
+    INTEREST_MODEL,
   };
 }
