f583a654d84e5e120bde52254bd645e85f9257c1
denismaxim0v <denis_maxim0v@protonmail.com>
2022-02-10 17:53:19 +0200

    wip: more tests

--- contracts/lend/tests/deposit_redeem.rs
+++ contracts/lend/tests/deposit_redeem.rs
@@ -1,6 +1,9 @@
 use lend_shared::{
     fadroma::{
-        decimal::one_token, ensemble::MockEnv, snip20_impl::msg::HandleMsg as Snip20HandleMsg,
+        decimal::one_token,
+        ensemble::MockEnv,
+        snip20_impl::msg::QueryAnswer,
+        snip20_impl::msg::{HandleMsg as Snip20HandleMsg, QueryMsg as Snip20QueryMsg},
         to_binary, Decimal256, Permit, Uint128, Uint256,
     },
     interfaces::{market, overseer},
@@ -223,4 +226,110 @@ fn redeem_underlying_basic() {
         .unwrap();
 
     assert_eq!(res.sl_token_balance, Uint256::from(0u128));
+}
+
+#[test]
+fn redeem_partial() {
+    // deposit 50e18
+    let deposit_amount = Uint128(50_000_000_000_000_000_000);
+    // redeem partial: 250e8
+    let redeem_amount = Uint128(250_000_000_00);
+    // exchange rate 1e9
+    let exchange_rate = Decimal256::from_uint256(1_000_000_000u128).unwrap();
+    let mut lend = Lend::default();
+
+    let underlying_1 = lend.new_underlying_token("ONE", 6).unwrap();
+
+    let market = lend
+        .whitelist_market(
+            underlying_1.clone(),
+            Decimal256::percent(50),
+            Some(exchange_rate),
+        )
+        .unwrap();
+
+    lend.ensemble
+        .execute(
+            &overseer::HandleMsg::Enter {
+                markets: vec![market.contract.address.clone()],
+            },
+            MockEnv::new(ADMIN, lend.overseer.clone()),
+        )
+        .unwrap();
+
+    lend.prefund_and_deposit(BORROWER, deposit_amount, market.contract.address.clone());
+
+    let res: market::AccountInfo = lend
+        .ensemble
+        .query(
+            market.contract.address.clone(),
+            &market::QueryMsg::Account {
+                method: Permit::<market::MarketPermissions>::new(
+                    BORROWER,
+                    vec![market::MarketPermissions::AccountInfo],
+                    vec![market.contract.address.clone()],
+                    "balance",
+                )
+                .into(),
+                block: None,
+            },
+        )
+        .unwrap();
+
+    // slToken balance should be 500e8
+    assert_eq!(res.sl_token_balance, Uint256::from(500_000_000_00u128));
+
+    // Redeem 250e8
+    lend.ensemble
+        .execute(
+            &market::HandleMsg::RedeemToken {
+                burn_amount: Uint256::from(redeem_amount),
+            },
+            MockEnv::new(BORROWER, market.contract.clone()),
+        )
+        .unwrap();
+
+    let res: market::AccountInfo = lend
+        .ensemble
+        .query(
+            market.contract.address.clone(),
+            &market::QueryMsg::Account {
+                method: Permit::<market::MarketPermissions>::new(
+                    BORROWER,
+                    vec![market::MarketPermissions::AccountInfo],
+                    vec![market.contract.address.clone()],
+                    "balance",
+                )
+                .into(),
+                block: None,
+            },
+        )
+        .unwrap();
+
+    assert_eq!(res.sl_token_balance, Uint256::from(redeem_amount));
+
+    lend.ensemble
+        .execute(
+            &Snip20HandleMsg::SetViewingKey {
+                key: "whatever".into(),
+                padding: None,
+            },
+            MockEnv::new(BORROWER, underlying_1.clone()),
+        )
+        .unwrap();
+
+    // underlying balance should be 25e18
+    if let QueryAnswer::Balance { amount } = lend
+        .ensemble
+        .query(
+            underlying_1.address.clone(),
+            Snip20QueryMsg::Balance {
+                address: BORROWER.into(),
+                key: "whatever".into(),
+            },
+        )
+        .unwrap()
+    {
+        assert_eq!(amount, Uint128(25 * one_token(18)));
+    }
 }
--- contracts/lend/tests/liquidate.rs
+++ contracts/lend/tests/liquidate.rs
@@ -194,3 +194,189 @@ fn liquidate_basic() {
         Uint256::from(3_888_000_000_000_000_000u128)
     );
 }
+
+#[test]
+fn liquidate_gain_interest() {
+    let mut lend = Lend::default();
+    let borrow_amount = Uint256::from(1 * one_token(18));
+
+    let underlying_1 = lend.new_underlying_token("ONE", 18).unwrap();
+    let underlying_2 = lend.new_underlying_token("TWO", 18).unwrap();
+
+    // whitelist markets
+    let market_1 = lend
+        .whitelist_market(
+            underlying_1.clone(),
+            Decimal256::percent(75),
+            Some(Decimal256::percent(50)),
+        )
+        .unwrap();
+
+    let market_2 = lend
+        .whitelist_market(
+            underlying_2.clone(),
+            Decimal256::percent(75),
+            Some(Decimal256::percent(50)),
+        )
+        .unwrap();
+
+    // set underlying prices
+    lend.set_oracle_price(market_1.symbol.as_bytes(), Uint128(1 * one_token(18)))
+        .unwrap();
+    lend.set_oracle_price(market_2.symbol.as_bytes(), Uint128(1 * one_token(18)))
+        .unwrap();
+
+    // prefund markets
+    lend.prefund_and_deposit(
+        BOB,
+        Uint128(2 * one_token(18)),
+        market_1.contract.address.clone(),
+    );
+    lend.prefund_user(ALICE, Uint128(20 * one_token(18)), underlying_2.clone());
+    // deposit
+    lend.ensemble
+        .execute(
+            &Snip20HandleMsg::Send {
+                recipient: market_2.contract.address.clone(),
+                recipient_code_hash: None,
+                amount: Uint128(10 * one_token(18)),
+                msg: Some(to_binary(&market::ReceiverCallbackMsg::Deposit {}).unwrap()),
+                memo: None,
+                padding: None,
+            },
+            MockEnv::new(ALICE, underlying_2.clone()),
+        )
+        .unwrap();
+
+    // enter markets
+    lend.ensemble
+        .execute(
+            &overseer::HandleMsg::Enter {
+                markets: vec![
+                    market_1.contract.address.clone(),
+                    market_2.contract.address.clone(),
+                ],
+            },
+            MockEnv::new(BOB, lend.overseer.clone()),
+        )
+        .unwrap();
+    lend.ensemble
+        .execute(
+            &overseer::HandleMsg::Enter {
+                markets: vec![
+                    market_1.contract.address.clone(),
+                    market_2.contract.address.clone(),
+                ],
+            },
+            MockEnv::new(ALICE, lend.overseer.clone()),
+        )
+        .unwrap();
+
+    lend.ensemble
+        .execute(
+            &market::HandleMsg::Borrow {
+                amount: borrow_amount.into(),
+            },
+            MockEnv::new(BOB, market_2.contract.clone()),
+        )
+        .unwrap();
+
+    let res: market::AccountInfo = lend
+        .ensemble
+        .query(
+            market_2.contract.address.clone(),
+            &market::QueryMsg::Account {
+                method: Permit::<market::MarketPermissions>::new(
+                    BOB,
+                    vec![market::MarketPermissions::AccountInfo],
+                    vec![market_2.contract.address.clone()],
+                    "balance",
+                )
+                .into(),
+                block: None,
+            },
+        )
+        .unwrap();
+    assert_eq!(res.sl_token_balance, Uint256::from(0u128));
+    assert_eq!(res.borrow_balance, Uint256::from(1 * one_token(18)));
+
+    // crash the price of first token and liquidate
+    lend.set_oracle_price(market_1.symbol.as_bytes(), Uint128(8 * one_token(17)))
+        .unwrap();
+
+    let liquidity = lend
+        .get_liquidity(
+            BOB,
+            Some(market_2.contract.address.clone()),
+            Uint256::zero(),
+            borrow_amount.into(),
+            Some(12347),
+        )
+        .unwrap();
+
+    // negative liquidity
+    assert_ne!(liquidity.shortfall, Uint256::zero());
+
+    let id = lend.id(BOB, market_2.contract.address.clone());
+
+    // TODO: block height is ignored for some reason?
+    // let mock_env = MockEnv::new(ALICE, underlying_2.clone()).height(12347);
+    // lend.ensemble
+    //     .execute(
+    //         &Snip20HandleMsg::Send {
+    //             recipient: market_2.contract.address.clone(),
+    //             recipient_code_hash: None,
+    //             amount: Uint128(1_500_000_000_000_000_000),
+    //             msg: Some(
+    //                 to_binary(&market::ReceiverCallbackMsg::Liquidate {
+    //                     borrower: id,
+    //                     collateral: market_1.contract.address.clone(),
+    //                 })
+    //                 .unwrap(),
+    //             ),
+    //             memo: None,
+    //             padding: None,
+    //         },
+    //         mock_env,
+    //     )
+    //     .unwrap();
+
+    // let res: market::AccountInfo = lend
+    //     .ensemble
+    //     .query(
+    //         market_1.contract.address.clone(),
+    //         &market::QueryMsg::Account {
+    //             method: Permit::<market::MarketPermissions>::new(
+    //                 BOB,
+    //                 vec![market::MarketPermissions::AccountInfo],
+    //                 vec![market_1.contract.address.clone()],
+    //                 "balance",
+    //             )
+    //             .into(),
+    //             block: None,
+    //         },
+    //     )
+    //     .unwrap();
+    // assert_eq!(res.sl_token_balance, Uint256::from(0u128));
+
+    // let res: market::AccountInfo = lend
+    //     .ensemble
+    //     .query(
+    //         market_1.contract.address.clone(),
+    //         &market::QueryMsg::Account {
+    //             method: Permit::<market::MarketPermissions>::new(
+    //                 ALICE,
+    //                 vec![market::MarketPermissions::AccountInfo],
+    //                 vec![market_1.contract.address.clone()],
+    //                 "balance",
+    //             )
+    //             .into(),
+    //             block: Some(12347),
+    //         },
+    //     )
+    //     .unwrap();
+    // assert_eq!(
+    //     res.sl_token_balance,
+    //     Uint256::from(3_888_000_000_000_000_000u128)
+    // );
+}
