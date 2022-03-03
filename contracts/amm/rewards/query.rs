7ff8bdf9c2f9c9f292055aa745db92e40079d101
ivanp <ivan@nignite.com>
2022-03-03 14:04:09 +0100

    added query permits for rewards

--- contracts/amm/rewards/query.rs
+++ contracts/amm/rewards/query.rs
@@ -21,6 +21,11 @@ pub enum RewardsQuery {
         address: HumanAddr,
         key: String,
     },
+    WithPermit{
+        query: QueryWithPermit,
+        permit: Permit<RewardsPermissions>
+    },
+
     /// For a moment in time, report pool status, with embedded clock status
     PoolInfo { at: Moment },
 }
@@ -35,9 +40,15 @@ where
         match self {
             RewardsQuery::Config => RewardsResponse::config(core),
             RewardsQuery::UserInfo { at, address, key } => {
-                RewardsResponse::user_info(core, at, address, key)
+                RewardsResponse::user_info_with_vk(core, at, &address, key)
             }
             RewardsQuery::PoolInfo { at } => RewardsResponse::pool_info(core, at),
+            RewardsQuery::WithPermit{query, permit} =>  {
+                match query {
+                    QueryWithPermit::UserInfo{ at} => RewardsResponse::user_info(core, at, &permit.address)
+                }
+                
+            }
         }
     }
 }
@@ -56,7 +67,8 @@ where
     C: Rewards<S, A, Q>,
 {
     /// Populate a response with account + pool + epoch info
-    fn user_info(core: &C, time: Moment, address: HumanAddr, key: String) -> StdResult<Self>;
+    fn user_info_with_vk(core: &C, time: Moment, address: &HumanAddr, key: String) -> StdResult<Self>;
+    fn user_info(core: &C, time: Moment, address: &HumanAddr) -> StdResult<Self>;
     /// Populate a response with pool + epoch info
     fn pool_info(core: &C, time: Moment) -> StdResult<Self>;
     /// Populate a response with the contract's configuration
@@ -69,16 +81,33 @@ where
     Q: Querier,
     C: Rewards<S, A, Q>,
 {
-    fn user_info(core: &C, time: Moment, address: HumanAddr, key: String) -> StdResult<Self> {
-        Auth::check_vk(core, &address, &key.into())?;
+    fn user_info_with_vk(core: &C, time: Moment, address: &HumanAddr, key: String) -> StdResult<Self> {
+        Auth::check_vk(core, address, &key.into())?;
         Ok(RewardsResponse::UserInfo(Account::from_addr(
-            core, &address, time,
+            core, address, time,
         )?))
     }
+    fn user_info(core: &C, time: Moment, address: &HumanAddr) -> StdResult<Self> {
+        Ok(RewardsResponse::UserInfo(Account::from_addr(core, address, time)?))
+    }
+
+    
     fn pool_info(core: &C, time: Moment) -> StdResult<RewardsResponse> {
         Ok(RewardsResponse::PoolInfo(Total::from_time(core, time)?))
     }
     fn config(core: &C) -> StdResult<RewardsResponse> {
         Ok(RewardsResponse::Config(RewardsConfig::from_storage(core)?))
     }
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryWithPermit {
+    UserInfo{ at: Moment},
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum RewardsPermissions {
+    UserInfo
 }
