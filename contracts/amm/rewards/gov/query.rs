ce5b489f8470d9e9986d92eb31923be71a41a57d
ivanp <ivan@nignite.com>
2022-03-04 14:12:11 +0100

    added query permits for governance

--- contracts/amm/rewards/gov/query.rs
+++ contracts/amm/rewards/gov/query.rs
@@ -1,4 +1,5 @@
-use fadroma::*;
+
+use fadroma::{HumanAddr, QueryDispatch, Storage, Api, Querier, StdResult};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -6,7 +7,7 @@ use super::{
     governance::Governance,
     response::{GovernanceResponse, IGovernanceResponse},
 };
-use crate::auth::Auth;
+use crate::{auth::{Auth, AuthMethod}, permit::Permit};
 use crate::time_utils::Moment;
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize, JsonSchema)]
@@ -26,6 +27,10 @@ pub enum GovernanceQuery {
         poll_id: u64,
         address: HumanAddr,
         key: String,
+    },
+    WithPermit {
+        query: QueryWithPermit,
+        permit: Permit<GovernancePermissions>
     },
     Config {},
 }
@@ -53,7 +58,26 @@ where
             } => {
                 Auth::check_vk(core, &address, &key.into())?;
                 GovernanceResponse::vote_status(core, poll_id, &address)
+            },
+            GovernanceQuery::WithPermit {permit, query} => {
+                let addr = Auth::authenticate(core, AuthMethod::Permit(permit), GovernancePermissions::VoteStatus)?;
+                match query {
+                    QueryWithPermit::VoteStatus{poll_id} => GovernanceResponse::vote_status(core, poll_id, &addr)
+                }
             }
         }
     }
+}
+
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryWithPermit {
+    VoteStatus { poll_id: u64},
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum GovernancePermissions {
+    VoteStatus
 }
