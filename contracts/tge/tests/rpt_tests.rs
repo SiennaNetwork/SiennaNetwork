393d9642aa5852d2254865313e854661c11b0adf
Wiz1991 <wizoftime92@gmail.com>
2022-03-18 14:38:09 +0100

    distribution update tests

--- contracts/tge/tests/rpt_tests.rs
+++ contracts/tge/tests/rpt_tests.rs
@@ -15,7 +15,7 @@ fn test_rpt_init() {
 }
 
 #[test]
-fn test_update_config() {
+fn test_admin_update_config() {
     let mut tge = TGE::default();
     let token1 = HumanAddr::from("secret1TOKEN1");
     let token2 = HumanAddr::from("secret1TOKEN2");
@@ -27,7 +27,7 @@ fn test_update_config() {
     tge.ensemble
         .execute(
             &sienna_rpt::HandleMsg::SetDistribution {
-                distribution: updated_distribution,
+                distribution: updated_distribution.clone(),
             },
             MockEnv::new(
                 ADMIN,
@@ -39,5 +39,35 @@ fn test_update_config() {
         )
         .unwrap();
 
-    // let response = tge.ensemble.query(tge.rpt.address, &sienna_rpt::QueryMsg)
+    let response: sienna_rpt::ConfigResponse = tge
+        .ensemble
+        .query(tge.rpt.address, &sienna_rpt::QueryMsg::Configuration {})
+        .unwrap();
+
+    assert_eq!(response.distribution, updated_distribution);
+}
+#[test]
+fn test_stranger_cant_update_config() {
+    let mut tge = TGE::default();
+    let token1 = HumanAddr::from("secret1TOKEN1");
+    let token2 = HumanAddr::from("secret1TOKEN2");
+    let updated_distribution = LinearMap(vec![
+        (token1.clone(), Uint128::from(1000u128)),
+        (token2.clone(), Uint128::from(1500u128)),
+    ]);
+
+    tge.ensemble
+        .execute(
+            &sienna_rpt::HandleMsg::SetDistribution {
+                distribution: updated_distribution.clone(),
+            },
+            MockEnv::new(
+                "stranger",
+                ContractLink {
+                    address: "RPT_CONTRACT".into(),
+                    code_hash: tge.rpt.code_hash,
+                },
+            ),
+        )
+        .unwrap_err();
 }
